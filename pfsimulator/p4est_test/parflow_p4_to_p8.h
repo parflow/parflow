ec803aaed78ffe5ad23a5aaa0bf6345497305af9
Jose Fonseca <fonseca@ins.uni-bonn.de>
2016-04-06 15:07:57 +0000

    add header to switch from 2d and 3d code
    git-svn-id: https://parflow.svn.cloudforge.com/parflow/parflow/branches/adaptive@763 39af3fb0-ca4e-0410-adf9-b4ae76d04516

---
+++ pfsimulator/p4est_test/parflow_p4_to_p8.h
@@ -0,0 +1,9 @@
+#ifndef PARFLOW_P4_TO_P8_H
+#define PARFLOW_P4_TO_P8_H
+
+#include <p4est_to_p8est.h>
+
+#define parflow_p4est_grid_2d_new         parflow_p4est_grid_3d_new
+#define parflow_p4est_grid_2d_destroy     parflow_p4est_grid_3d_destroy
+
+#endif /* !PARFLOW_P4_TO_P8_H */
