20be7d212750a42980e595d5ceb27bf878ce7d90
Jose Fonseca <fonseca@ins.uni-bonn.de>
2016-04-06 15:07:48 +0000

    collect some math functions
    git-svn-id: https://parflow.svn.cloudforge.com/parflow/parflow/branches/adaptive@762 39af3fb0-ca4e-0410-adf9-b4ae76d04516

---
+++ pfsimulator/p4est_test/parflow_p4est_math.c
@@ -0,0 +1,24 @@
+#include <math.h>
+
+int
+gcd (int a, int b)
+{
+  int                 c;
+  while (a) {
+    c = a;
+    a = b % a;
+    b = c;
+  }
+  return b;
+}
+
+int
+powtwo_div (int a)
+{
+  int                 c = 0;
+  while (!(a % 2)) {
+    c++;
+    a /= 2;
+  }
+  return (int) pow (2, c);
+}
---
+++ pfsimulator/p4est_test/parflow_p4est_math.h
@@ -0,0 +1,7 @@
+#ifndef PARLOW_P4EST_MATH_H
+#define PARFLOW_P4EST_MATH_H
+
+int                 gcd (int a, int b);
+int                 powtwo_div (int a);
+
+#endif // !PARFLOW_P4EST_MATH_H
