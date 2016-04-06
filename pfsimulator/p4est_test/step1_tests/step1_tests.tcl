a4b9c04423cb4bfbb36455583ef1c4c932eed0aa
Jose Fonseca <fonseca@ins.uni-bonn.de>
2016-04-06 15:29:14 +0000

    tcl driver ready for tests 1-4
    git-svn-id: https://parflow.svn.cloudforge.com/parflow/parflow/branches/adaptive@880 39af3fb0-ca4e-0410-adf9-b4ae76d04516

--- pfsimulator/p4est_test/step1_tests/step1_tests.tcl
+++ pfsimulator/p4est_test/step1_tests/step1_tests.tcl
@@ -2,7 +2,81 @@ lappend auto_path $env(PARFLOW_DIR)/bin
 package require parflow
 namespace import Parflow::*
 
-# each quoted text is a combination of P,Q,R
+# Test cases for step 1 in parflow-p4est integration.
+#
+# The "test_brick_2d" script sets up the database for
+# to execute the default version of Parflow and take the Processor
+# grid (P,Q,R) as argument.
+#
+# The "test_brick_2d_with_p4est"  sets up the database to execute
+# the Parflow version with p4est integrated and
+# take as arguments the number of subgrid points in each
+# coordinate direction (Mx, My ,Mz)
+
+#	TEST 1
+#
+#  +-----+-----+
+#  |     |     |
+#  |     |     |
+#  +-----+-----+
+#
+#  Nx = Ny = 12
+#  PARLOW		PARFLOW + P4EST
+#  P=2, Q=1, R=1	Mx=6 My=12, Mz=1  --> 2 Trees and initial level 0
+
+#	TEST 2
+#
+#  +-----+-----+
+#  |     |     |
+#  |     |     |
+#  +-----+-----+
+#  |     |     |
+#  |     |     |
+#  +-----+-----+
+#
+#  Nx = Ny = 12
+#  PARLOW		PARFLOW + P4EST
+#  P=2, Q=2, R=1	Mx=6 My=6, Mz=1  --> 1 Trees and initial level 1
+
+#	TEST 3
+#
+#  +-----+-----+-----+-----+
+#  |     |     |     |	   |
+#  |     |     |     |	   |
+#  +-----+-----+-----+-----+
+#  |     |     |     |	   |
+#  |     |     |     |	   |
+#  +-----+-----+-----+-----+
+#  |     |     |     |     |
+#  |     |     |     |     |
+#  +-----+-----+-----+-----+
+#
+#  Nx = Ny = 12
+#  PARLOW		PARFLOW + P4EST
+#  P=4, Q=3, R=1	Mx=3 My=4, Mz=1 --> 12 Trees and initial level 0
+
+
+#	TEST 4
+#  +-----+-----+-----+-----+
+#  |     |     |     |	   |
+#  |     |     |     |	   |
+#  +-----+-----+-----+-----+
+#  |     |     |     |	   |
+#  |     |     |     |	   |
+#  +-----+-----+-----+-----+
+#  |     |     |     |	   |
+#  |     |     |     |	   |
+#  +-----+-----+-----+-----+
+#  |     |     |     |     |
+#  |     |     |     |     |
+#  +-----+-----+-----+-----+
+#
+#  Nx = Ny = 12
+#  PARLOW		PARFLOW + P4EST
+#  P=4, Q=4, R=1	Mx=3 My=3, Mz=1 --> 1 Trees and initial level 2
+
+
+# each quoted text is a combination of P,Q,R as described above
 array set pf_param { 
 	1 "2 1 1" 
 	2 "2 2 1" 
@@ -10,7 +84,7 @@ array set pf_param {
 	4 "4 4 1"
 }
 
-# each quoted text is a combination of Mx,My,Mz
+# each quoted text is a combination of Mx,My,Mz as described above
 array set pf_param_with_p4est { 
 	1 "6 12 1" 
 	2 "6 6 1" 
@@ -18,7 +92,7 @@ array set pf_param_with_p4est {
 	4 "3 3 1"
 }
 
-# Required number of processors for the mpirun
+# Required number of processors for each case
 array set np_arr {
 	1 "2"
 	2 "4"
@@ -26,12 +100,11 @@ array set np_arr {
 	4 "16" 
 }
 
-for {set i 3} {$i < 5} {incr i} {
+for {set i 1} {$i < 5} {incr i} {
 
 	#Purge output from previous runs
-	exec rm -rf "output_test${i}"
-	exec mkdir -p output_test${i}/test_brick_2d.out/
-	exec mkdir -p output_test${i}/test_brick_2d_with_p4est.out/
+        exec rm -rf "output_test${i}"
+        exec mkdir  output_test${i}
 
 	#Update parflow data base for each case
 	set  PQR [split $pf_param($i) ]
@@ -41,17 +114,21 @@ for {set i 3} {$i < 5} {incr i} {
 	exec tclsh test_brick_2d_with_p4est.tcl\
 		[lindex $Mxyz 0] [lindex $Mxyz 1] [lindex $Mxyz 2]
 	
+        #Move the created data base to output directory
+        exec mv {*}[glob *.pfidb] output_test${i}/
+
 	#run each example
 	puts "Running TEST $i"
-	exec mpirun -np $np_arr($i) ../test_grid test_brick_2d
-	exec mpirun -np $np_arr($i) ../test_grid test_brick_2d_with_p4est
+        cd  output_test${i}/
+        exec mpirun -np $np_arr($i) ../../test_grid test_brick_2d
+        exec mpirun -np $np_arr($i) ../../test_grid test_brick_2d_with_p4est
 
 	#Colapse paralell output in single files
 	pfundist test_brick_2d
 	pfundist test_brick_2d_with_p4est
 	
 	#Compare pressure output file for both test cases	
-	source compare_files.tcl
+        source ../compare_files.tcl
 	set passed 1
 	
 	foreach t "00000 00001" {
@@ -68,10 +145,6 @@ for {set i 3} {$i < 5} {incr i} {
 		puts "FAILED"
 	}
 
-	# move output to suitable subdirectories for further use
-	exec mv -f test_brick_2d.out/\
-		output_test${i}/test_brick_2d.out/
-	exec mv -f test_brick_2d_with_p4est.out/\
-		output_test${i}/test_brick_2d_with_p4est.out
-	exec mv {*}[glob *.out*] output_test${i}
+
+        cd ..
 }
