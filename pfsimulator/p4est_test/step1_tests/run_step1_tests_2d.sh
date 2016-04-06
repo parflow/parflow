e6a639820c848d07a6322a1bced896d112be0b05
Jose Fonseca <fonseca@ins.uni-bonn.de>
2016-04-06 15:29:35 +0000

    add additional test
    git-svn-id: https://parflow.svn.cloudforge.com/parflow/parflow/branches/adaptive@882 39af3fb0-ca4e-0410-adf9-b4ae76d04516

--- pfsimulator/p4est_test/step1_tests/run_step1_tests_2d.sh
+++ pfsimulator/p4est_test/step1_tests/run_step1_tests_2d.sh
@@ -3,13 +3,14 @@
 # Test cases for step 1 in parflow-p4est integration.
 #
 # The "test_brick_2d" script sets up the database for
-# to execute the default version of Parflow and take the Processor
-# grid (P,Q,R) as argument.
+# to execute the default version of Parflow and take
+# the number of grid points (Nx,Ny) and the Processor
+# grid (P,Q) as argument.
 #
 # The "test_brick_2d_with_p4est"  sets up the database to execute
 # the Parflow version with p4est integrated and
-# take as arguments the number of subgrid points in each
-# coordinate direction (Mx, My ,Mz)
+# take as arguments the number of grid points (Nx,Ny) and
+# subgrid points in each coordinate direction (Mx, My).
 
 #	TEST 1 
 #
@@ -20,41 +21,55 @@
 #
 #  Nx = Ny = 12
 #  PARLOW		PARFLOW + P4EST
-#  P=2, Q=1, R=1	Mx=6 My=12, Mz=1  --> 2 Trees and initial level 0
+#  P=2, Q=1     	Mx=6 My=12    --> 2 Trees and initial level 0
 
 #	TEST 2
 #
-#  +-----+-----+
-#  |     |     |
-#  |     |     |
-#  +-----+-----+
-#  |     |     |
-#  |     |     |
-#  +-----+-----+
+#  +-----+-----+-----+-----+
+#  |     |     |     |     |
+#  |     |     |     |     |
+#  +-----+-----+-----+-----+
 #
-#  Nx = Ny = 12
+#  Nx = Ny = 14
 #  PARLOW		PARFLOW + P4EST
-#  P=2, Q=2, R=1	Mx=6 My=6, Mz=1  --> 1 Trees and initial level 1
+#  P=4, Q=1     	Mx=3 My=14    --> 4 Trees and initial level 0
+#
+#  This case is important to test that the lower left corner of each
+#  Subgrid has been computed correctly.
 
 #	TEST 3
 #
-#  +-----+-----+-----+-----+
-#  |     |     |     |	   |
-#  |     |     |     |	   |
-#  +-----+-----+-----+-----+
-#  |     |     |     |	   |
-#  |     |     |     |	   |
-#  +-----+-----+-----+-----+
-#  |     |     |     |     |
-#  |     |     |     |     |
-#  +-----+-----+-----+-----+
+#  +-----+-----+
+#  |     |     |
+#  |     |     |
+#  +-----+-----+
+#  |     |     |
+#  |     |     |
+#  +-----+-----+
 #
 #  Nx = Ny = 12
 #  PARLOW		PARFLOW + P4EST
-#  P=4, Q=3, R=1	Mx=3 My=4, Mz=1 --> 12 Trees and initial level 0
-
+#  P=2, Q=2     	Mx=6 My=6   --> 1 Trees and initial level 1
 
 #	TEST 4
+#
+#  +-----+-----+-----+-----+
+#  |     |     |     |	   |
+#  |     |     |     |	   |
+#  +-----+-----+-----+-----+
+#  |     |     |     |	   |
+#  |     |     |     |	   |
+#  +-----+-----+-----+-----+
+#  |     |     |     |     |
+#  |     |     |     |     |
+#  +-----+-----+-----+-----+
+#
+#  Nx = Ny = 12
+#  PARLOW		PARFLOW + P4EST
+#  P=4, Q=3     	Mx=3 My=4   --> 12 Trees and initial level 0
+
+
+#	TEST 5
 #  +-----+-----+-----+-----+
 #  |     |     |     |	   |
 #  |     |     |     |	   |
@@ -71,22 +86,25 @@
 #
 #  Nx = Ny = 12
 #  PARLOW		PARFLOW + P4EST
-#  P=4, Q=4, R=1	Mx=3 My=3, Mz=1 --> 1 Trees and initial level 2
+#  P=4, Q=4     	Mx=3 My=3   --> 1 Trees and initial level 2
 
 # This set the tolerance for the squared difference of the pressure L2 error
 # computed by parflow with and without using p4est.
 EPS=1e-6
 
-# each quoted text is a combination of P,Q,R
-pf_param=( "2 1 1" "2 2 1" "4 3 1" "4 4 1")
+# each quoted text is a combination of Nx,Ny
+pf_param_nxy=("12 12" "14 14" "12 12" "12 12" "12 12" "12 12")
 
-# each quoted text is a combination of Mx,My,Mz
-pf_param_with_p4est=( "6 12 1" "6 6 1" "3 4 1" "3 3 1")
+# each quoted text is a combination of P,Q
+pf_param_pq=("2 1" "4 1" "2 2" "4 3" "4 4")
+
+# each quoted text is a combination of Mx,My
+pf_param_mxy=("6 12" "3 14" "6 6" "3 4" "3 3")
 
 # Required number of processors for the mpirun
-np_arr=( 2 4 12 16 )
+np_arr=( 2 4 4 12 16 )
 
-for (( i=0;i<4;i++ )); do
+for (( i=0;i<5;i++ )); do
 
 	T=$(echo "${i}+1" | bc -l)
 
@@ -95,8 +113,8 @@ for (( i=0;i<4;i++ )); do
 	mkdir  output_test${T}
 
 	#Update parflow data base for each case
-	tclsh test_brick_2d.tcl ${pf_param[i]}
-	tclsh test_brick_2d_with_p4est.tcl ${pf_param_with_p4est[i]}
+        tclsh test_brick_2d.tcl ${pf_param_nxy[i]} ${pf_param_pq[i]}
+        tclsh test_brick_2d_with_p4est.tcl ${pf_param_nxy[i]} ${pf_param_mxy[i]}
 
 	#Move the created data base to output directory
 	mv *.pfidb output_test${T}/
--- pfsimulator/p4est_test/step1_tests/step1_tests.tcl
+++ pfsimulator/p4est_test/step1_tests/step1_tests.tcl
@@ -5,13 +5,14 @@ namespace import Parflow::*
 # Test cases for step 1 in parflow-p4est integration.
 #
 # The "test_brick_2d" script sets up the database for
-# to execute the default version of Parflow and take the Processor
-# grid (P,Q,R) as argument.
+# to execute the default version of Parflow and take
+# the number of grid points (Nx,Ny) and the Processor
+# grid (P,Q) as argument.
 #
 # The "test_brick_2d_with_p4est"  sets up the database to execute
 # the Parflow version with p4est integrated and
-# take as arguments the number of subgrid points in each
-# coordinate direction (Mx, My ,Mz)
+# take as arguments the number of grid points (Nx,Ny) and
+# subgrid points in each coordinate direction (Mx, My).
 
 #	TEST 1
 #
@@ -22,41 +23,55 @@ namespace import Parflow::*
 #
 #  Nx = Ny = 12
 #  PARLOW		PARFLOW + P4EST
-#  P=2, Q=1, R=1	Mx=6 My=12, Mz=1  --> 2 Trees and initial level 0
+#  P=2, Q=1     	Mx=6 My=12,    --> 2 Trees and initial level 0
 
 #	TEST 2
 #
-#  +-----+-----+
-#  |     |     |
-#  |     |     |
-#  +-----+-----+
-#  |     |     |
-#  |     |     |
-#  +-----+-----+
+#  +-----+-----+-----+-----+
+#  |     |     |     |     |
+#  |     |     |     |     |
+#  +-----+-----+-----+-----+
 #
-#  Nx = Ny = 12
+#  Nx = Ny = 14
 #  PARLOW		PARFLOW + P4EST
-#  P=2, Q=2, R=1	Mx=6 My=6, Mz=1  --> 1 Trees and initial level 1
+#  P=4, Q=1     	Mx=3 My=14,    --> 4 Trees and initial level 0
+#
+#  This case is important to test that the lower left corner of each
+#  Subgrid has been computed correctly.
 
 #	TEST 3
 #
-#  +-----+-----+-----+-----+
-#  |     |     |     |	   |
-#  |     |     |     |	   |
-#  +-----+-----+-----+-----+
-#  |     |     |     |	   |
-#  |     |     |     |	   |
-#  +-----+-----+-----+-----+
-#  |     |     |     |     |
-#  |     |     |     |     |
-#  +-----+-----+-----+-----+
+#  +-----+-----+
+#  |     |     |
+#  |     |     |
+#  +-----+-----+
+#  |     |     |
+#  |     |     |
+#  +-----+-----+
 #
 #  Nx = Ny = 12
 #  PARLOW		PARFLOW + P4EST
-#  P=4, Q=3, R=1	Mx=3 My=4, Mz=1 --> 12 Trees and initial level 0
-
+#  P=2, Q=2     	Mx=6 My=6     --> 1 Trees and initial level 1
 
 #	TEST 4
+#
+#  +-----+-----+-----+-----+
+#  |     |     |     |	   |
+#  |     |     |     |	   |
+#  +-----+-----+-----+-----+
+#  |     |     |     |	   |
+#  |     |     |     |	   |
+#  +-----+-----+-----+-----+
+#  |     |     |     |     |
+#  |     |     |     |     |
+#  +-----+-----+-----+-----+
+#
+#  Nx = Ny = 12
+#  PARLOW		PARFLOW + P4EST
+#  P=4, Q=3     	Mx=3 My=4    --> 12 Trees and initial level 0
+
+
+#	TEST 5
 #  +-----+-----+-----+-----+
 #  |     |     |     |	   |
 #  |     |     |     |	   |
@@ -73,46 +88,61 @@ namespace import Parflow::*
 #
 #  Nx = Ny = 12
 #  PARLOW		PARFLOW + P4EST
-#  P=4, Q=4, R=1	Mx=3 My=3, Mz=1 --> 1 Trees and initial level 2
+#  P=4, Q=4     	Mx=3 My=3    --> 1 Trees and initial level 2
 
 
-# each quoted text is a combination of P,Q,R as described above
-array set pf_param { 
-	1 "2 1 1" 
-	2 "2 2 1" 
-	3 "4 3 1"
-	4 "4 4 1"
+# each quoted text is a combination of Nx,Ny as described above
+array set pf_param_nxy {
+        1 "12 12"
+        2 "14 14"
+        3 "12 12"
+        4 "12 12"
+        5 "12 12"
 }
 
-# each quoted text is a combination of Mx,My,Mz as described above
-array set pf_param_with_p4est { 
-	1 "6 12 1" 
-	2 "6 6 1" 
-	3 "3 4 1" 
-	4 "3 3 1"
+# each quoted text is a combination of P,Q as described above
+array set pf_param_pq {
+        1 "2 1"
+        2 "4 1"
+        3 "2 2"
+        4 "4 3"
+        5 "4 4"
+}
+
+# each quoted text is a combination of Mx,My as described above
+array set pf_param_mxy {
+        1 "6 12"
+        2 "3 14"
+        3 "6 6"
+        4 "3 4"
+        5 "3 3"
 }
 
 # Required number of processors for each case
 array set np_arr {
 	1 "2"
-	2 "4"
-	3 "12"
-	4 "16" 
+        2 "4"
+        3 "4"
+        4 "12"
+        5 "16"
 }
 
-for {set i 1} {$i < 5} {incr i} {
+for {set i 1} {$i < 6} {incr i} {
 
 	#Purge output from previous runs
         exec rm -rf "output_test${i}"
         exec mkdir  output_test${i}
 
 	#Update parflow data base for each case
-	set  PQR [split $pf_param($i) ]
+        set  Nxy [split $pf_param_nxy($i) ]
+        set  PQ [split $pf_param_pq($i) ]
 	exec tclsh test_brick_2d.tcl\
-		[lindex $PQR 0] [lindex $PQR 1] [lindex $PQR 2]
-	set  Mxyz [split $pf_param_with_p4est($i)]
+                [lindex $Nxy 0] [lindex $Nxy 1]\
+                [lindex $PQ 0] [lindex $PQ 1]
+        set  Mxy [split $pf_param_mxy($i)]
 	exec tclsh test_brick_2d_with_p4est.tcl\
-		[lindex $Mxyz 0] [lindex $Mxyz 1] [lindex $Mxyz 2]
+                [lindex $Nxy 0] [lindex $Nxy 1]\
+                [lindex $Mxy 0] [lindex $Mxy 1]
 	
         #Move the created data base to output directory
         exec mv {*}[glob *.pfidb] output_test${i}/
@@ -140,9 +170,9 @@ for {set i 1} {$i < 5} {incr i} {
 	}
 
 	if $passed {
-		puts "PASSED"
+                puts "PASSED\n"
 	} {
-		puts "FAILED"
+                puts "FAILED\n"
 	}
 
 
