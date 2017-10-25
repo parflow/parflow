9edfcff57d4cd6d3616260e7b90573d32243960f
xy124 <qcd@gmx.net>
2017-10-25 21:37:33 +0200

    Rewrite functionality of write_parflow_netcdf.c
    Code in write_parflow_netcdf.c is way to hacky and hard to understand.
    Thus we implement the netcdf writer more or less from scratch.

--- flowvr/netcdf-writer/main.c
+++ flowvr/netcdf-writer/main.c
@@ -1,22 +1,36 @@
 #include <fca/fca.h>
+#include "parflow_config.h"
 #include "../../pfsimulator/parflow_lib/parflow.h"
 #include "../../pfsimulator/parflow_lib/parflow_netcdf.h"
 #include "../../pfsimulator/parflow_lib/GridMessage.h"
+#include <netcdf_par.h>
+#include <netcdf.h>
+#include <stdio.h>
+#include <assert.h>
 
-void writeTime(double *time, int ncID) {
-  long end[MAX_NC_VARS];
-  varNCData *myVarNCData;
-  int myVarID=LookUpInventory("time", &myVarNCData);  //TODO: set variable!
-  size_t start[myVarNCData->dimSize], count[myVarNCData->dimSize];
-  nc_var_par_access(ncID, myVarID, NC_COLLECTIVE);
-  find_variable_length(ncID, myVarID, end);
-  start[0] = end[0]; count[0] = 1;
-  //start[0] = counter; count[0] = 1;
-  int status = nc_put_vara_double(ncID, myVarID, start, count, time);
+#include "../../pfsimulator/amps/mpi1/amps.h"
+
+
+int CreateFile(const char* filename, int nX, int nY, int nZ, int *pxID, int *pyID, int *pzID, int *ptimeID) {
+  int ncID = 0;
+  if (nc_create_par(filename, NC_NETCDF4|NC_MPIIO, amps_CommWrite, MPI_INFO_NULL, &ncID) != NC_NOERR)
+  {
+    PARFLOW_ERROR("Could not create File!");
+  }
+
+  // Add Dimensions
+  nc_def_dim(ncID, "x", nX, pxID);
+  nc_def_dim(ncID, "y", nY, pyID);
+  nc_def_dim(ncID, "z", nZ, pzID);
+  nc_def_dim(ncID, "time", NC_UNLIMITED, ptimeID);  // unlimited dim should be first?! TODO?
+
+
+  return ncID;
 }
 
 int main (int argc , char *argv [])
 {
+  printf("starting netcdf-writer\n");
   // TODO
 
   // - Premessage sends out start message with standard timing ;)
@@ -28,72 +42,72 @@ int main (int argc , char *argv [])
   fca_port portPressureIn = fca_new_port("pressureIn", fca_IN, 0, NULL);
 
   fca_append_port(moduleNetCDFWriter, portPressureIn);
-  fca_port outPort = fca_new_port("outPort", fca_OUT, 0, NULL);
-  fca_append_port(moduleNetCDFWriter, outPort);
+//  fca_port outPort = fca_new_port("outPort", fca_OUT, 0, NULL);
+//  fca_append_port(moduleNetCDFWriter, outPort);
 
   const fca_stamp stampTime = fca_register_stamp(portPressureIn, "stampTime", fca_FLOAT);
   /*const fca_stamp stampN = fca_register_stamp(portPressureIn, "N", fca_INT);  // contains amount of cores that must be merged*/
   const fca_stamp stampFileNumber = fca_register_stamp(portPressureIn, "stampFileNumber",fca_INT);
-  const fca_stamp stampStartTime = fca_register_stamp(outPort, "stampStartTime", fca_FLOAT);
-  const fca_stamp stampStop = fca_register_stamp(outPort, "stampStopTime", fca_FLOAT);  // TODO good idea to use float? or should we put the double in the messages payload??
+//  const fca_stamp stampStartTime = fca_register_stamp(outPort, "stampStartTime", fca_FLOAT);
+//  const fca_stamp stampStop = fca_register_stamp(outPort, "stampStopTime", fca_FLOAT);  // TODO good idea to use float? or should we put the double in the messages payload??
 
+  if(!fca_init_module(moduleNetCDFWriter)){
+    printf("ERROR : init_module failed!\n");
+  }
 
+  int it = 0;
+  int currentFileID;
+  int xID, yID, zID, timeID;
   while (fca_wait(moduleNetCDFWriter)) {
+    ++it;
     fca_message msg = fca_get(portPressureIn);
     float *time = (float*)fca_read_stamp(msg, stampTime);
     int *filenumber = (int*)fca_read_stamp(msg, stampFileNumber);
 
-    int ncID;
 
 
-    long end[MAX_NC_VARS];
+    char file_name[1024];  // TODO: better filename generation!
+    sprintf(file_name, "out%d.nc", it); // atm: create a new file for each message group.
 
+    // to access the variables in the ncFile...
+    int pressureVarID;
+    int timeVarID;
 
-    const char* file_name = "out.nc"; // TODO: generate more intelligently
-    size_t pos = 0;
-    int myVarID;
-    do {
-      GridMessageMetadata* m = (GridMessageMetadata*)fca_get_read_access(msg, pos);
-      varNCData *myVarNCData;
-      if (pos == 0) {
-        CreateNCFileNode(file_name, m->nX, m->nY, m->nZ, &ncID);
-        writeTime(time, ncID);
-        int myVarID=LookUpInventory("pressure", &myVarNCData);  //TODO: set variable!
+    assert(fca_number_of_segments(msg)%2 == 0);
+    for (int i = 0; i < fca_number_of_segments(msg); i+=2) {
+      GridMessageMetadata* m = (GridMessageMetadata*)fca_get_read_access(msg, i);
+      if (i == 0) {
+        currentFileID = CreateFile(file_name, m->nX, m->nY, m->nZ, &xID, &yID, &zID, &timeID); // TODO: does not work when opening an existing file!
+        // add variable Time and pressure:
+        nc_def_var(currentFileID, "time", NC_DOUBLE, 1, &timeID, &timeVarID);
+        int pressure_dims[4] = {timeID, zID, yID, xID}; // low: why in this order? I guess so it will count in the right order ;)
+        nc_def_var(currentFileID, "pressure", NC_DOUBLE, 4, pressure_dims, &pressureVarID);
+        printf("Adding Time");
+
+        size_t start[1], count[1];
+        nc_var_par_access(currentFileID, timeVarID, NC_COLLECTIVE);
+        find_variable_length(currentFileID, timeVarID, start);
+        printf("start writing time at %d\n", start[0]);
+        count[0] = 1;  // writing one value
+        int status = nc_put_vara_double(currentFileID, timeVarID, start, count, time);
       }
 
-      nc_var_par_access(ncID, myVarID, NC_COLLECTIVE);
-      find_variable_length(ncID, myVarID, end);
-      size_t start[myVarNCData->dimSize], count[myVarNCData->dimSize];
-      start[0] = end[0]-1;
-      count[0] = 1;
-      start[1] = m->iz; start[2] =m->iy; start[3] = m->ix;
-      count[1] = m->nz; count[2] =m->ny; count[3] = m->nx;
-      pos += sizeof(GridMessageMetadata);
-      double const * const data = (double*)fca_get_read_access(msg, pos);
+      nc_var_par_access(currentFileID, pressureVarID, NC_COLLECTIVE);
+      // write next timestep
+      size_t start[4] = {0, m->iz, m->iy, m->ix};
+      size_t count[4] = {1, m->nz, m->ny, m->nx};
+      find_variable_length(currentFileID, timeVarID, &(start[0]));
+      start[0] = start[0] - 1;
+      double const * const data = (double*)fca_get_read_access(msg, i+1);
       // now do a write to the cdf! (! all the preparations up there are necessary!
-      int status = nc_put_vara_double(ncID, myVarID, start, count, data);
+      int status = nc_put_vara_double(currentFileID, pressureVarID, start, count, data);
+      printf("putting doubles");
 
-      pos += m->nx * m->ny * m->nz;
-    } while( pos < fca_get_segment_size(msg, 0) );
+    }
 
     // one file per timestep...
-    nc_close(ncID);
-
-
-
-
-    /*static int numStepsInFile = 1;*/
-    /*int userSpecSteps = GetInt("NetCDF.NumStepsPerFile");*/
-    /*static char file_name[255];*/
-    /*static int numOfDefVars=0;*/
-
-
-    //    all the steps go into one file :P
-
-    varNCData *myVarNCData;
-
-
-    // we are working directly with file nodes as this should be faster in the end when more than one process will be writing at the same time
+    nc_close(currentFileID);
+    printf("wrote %s\n", file_name);
 
 
     fca_free(msg);
@@ -103,5 +117,5 @@ int main (int argc , char *argv [])
   }
 
 
-
+  fca_free(moduleNetCDFWriter);
 }
