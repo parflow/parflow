10f0f083f846834db847de5c95ab2169315695dd
xy124 <qcd@gmx.net>
2017-10-30 14:21:31 +0100

    Implement multiple timesteps to one nc file for netcdf-writer
    Now the netcdf-writer can reopen output files to append new timesteps.
    
    Furthermore time values are now correctly converted from double to float
    and vice versa.

--- flowvr/netcdf-writer/main.c
+++ flowvr/netcdf-writer/main.c
@@ -7,6 +7,7 @@
 #include <netcdf.h>
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
 
 #include "../../pfsimulator/amps/mpi1/amps.h"
 
@@ -16,18 +17,46 @@
 #define D(x)
 #endif
 
-int CreateFile(const char* filename, int nX, int nY, int nZ, int *pxID, int *pyID, int *pzID, int *ptimeID) {
+// creates dimension or only returns its id if already existent.
+int getDim(int ncID, const char *name, size_t len, int *idp)
+{
+  int res = nc_def_dim(ncID, name, len, idp);
+  if (res == NC_ENAMEINUSE) {
+    res= nc_inq_varid(ncID, name, idp);
+  }
+  return res;
+}
+
+int CreateFile(const char* file_name, size_t nX, size_t nY, size_t nZ, int *pxID, int *pyID, int *pzID, int *ptimeID) {
+  // Opens/ creates file if not already there.
   int ncID = 0;
-  if (nc_create_par(filename, NC_NETCDF4|NC_MPIIO, amps_CommWrite, MPI_INFO_NULL, &ncID) != NC_NOERR)
+
+  if (access( file_name, F_OK ) == -1)
   {
-    PARFLOW_ERROR("Could not create File!");
+    if (nc_create_par(file_name, NC_NETCDF4|NC_MPIIO, amps_CommWrite, MPI_INFO_NULL, &ncID) != NC_NOERR)
+    {
+      D("Error creating file!")
+      PARFLOW_ERROR("Could not create file!");
+    }
+  }
+  else
+  {
+    // open it
+    D("file exists already. Opening it!");
+    if (nc_open_par(file_name, NC_MPIIO|NC_WRITE, amps_CommWorld, MPI_INFO_NULL, &ncID) != NC_NOERR)
+    {
+      D("Error opening existing file file!");
+      PARFLOW_ERROR("Could not open file!");
+    }
   }
 
-  // Add Dimensions
-  nc_def_dim(ncID, "x", nX, pxID);
-  nc_def_dim(ncID, "y", nY, pyID);
-  nc_def_dim(ncID, "z", nZ, pzID);
-  nc_def_dim(ncID, "time", NC_UNLIMITED, ptimeID);  // unlimited dim should be first?! TODO?
+  // create file. if it exists already, just load it.
+
+  // add/get Dimensions
+  getDim(ncID, "x", nX, pxID);
+  getDim(ncID, "y", nY, pyID);
+  getDim(ncID, "z", nZ, pzID);
+  getDim(ncID, "time", NC_UNLIMITED, ptimeID);  // unlimited dim should be first?! TODO?
 
 
   return ncID;
@@ -35,7 +64,14 @@ int CreateFile(const char* filename, int nX, int nY, int nZ, int *pxID, int *pyI
 
 int main (int argc , char *argv [])
 {
+
   printf("starting netcdf-writer\n");
+  MPI_Init(&argc, &argv);
+
+
+  /***********************
+   * init FlowVR Module
+   */
   fca_module moduleNetCDFWriter = fca_new_empty_module();
   fca_port portPressureIn = fca_new_port("pressureIn", fca_IN, 0, NULL);
 
@@ -44,31 +80,21 @@ int main (int argc , char *argv [])
   fca_port outPort = fca_new_port("outPort", fca_OUT, 0, NULL);
   fca_append_port(moduleNetCDFWriter, outPort);
 
-  const fca_stamp stampTime = fca_register_stamp(portPressureIn, "stampTime", fca_FLOAT);
-  /*const fca_stamp stampN = fca_register_stamp(portPressureIn, "N", fca_INT);  // contains amount of cores that must be merged*/
-  const fca_stamp stampFileNumber = fca_register_stamp(portPressureIn, "stampFileNumber",fca_INT);
-//  const fca_stamp stampStartTime = fca_register_stamp(outPort, "stampStartTime", fca_FLOAT);
-//  const fca_stamp stampStop = fca_register_stamp(outPort, "stampStopTime", fca_FLOAT);  // TODO good idea to use float? or should we put the double in the messages payload??
+  const fca_stamp stampTime = fca_register_stamp(portPressureIn, "stampTime", fca_FLOAT);// TODO good idea to use float? or should we put the double in the messages payload??
+  const fca_stamp stampFileName = fca_register_stamp(portPressureIn, "stampFileName", fca_STRING);
 
   if(!fca_init_module(moduleNetCDFWriter)){
     printf("ERROR : init_module failed!\n");
   }
 
-  int it = 0;
   int currentFileID;
   int xID, yID, zID, timeID;
   D("nowWaiting\n");
   while (fca_wait(moduleNetCDFWriter)) {
-    D("got some stuff to write\n"); // TODO: implement debug messages for more performance in release?
-    ++it;
+    D("got some stuff to write\n");
     fca_message msg = fca_get(portPressureIn);
-    float *time = (float*)fca_read_stamp(msg, stampTime);
-    int *filenumber = (int*)fca_read_stamp(msg, stampFileNumber);
-
-
-
-    char file_name[1024];  // TODO: better filename generation!
-    sprintf(file_name, "out%d.nc", it); // atm: create a new file for each message group.
+    double time = (double) *((float*) fca_read_stamp(msg, stampTime));
+    char *file_name = (char*) fca_read_stamp(msg, stampFileName);
 
     // to access the variables in the ncFile...
     int pressureVarID;
@@ -78,7 +104,7 @@ int main (int argc , char *argv [])
     for (int i = 0; i < fca_number_of_segments(msg); i+=2) {
       GridMessageMetadata* m = (GridMessageMetadata*)fca_get_read_access(msg, i);
       if (i == 0) {
-        currentFileID = CreateFile(file_name, m->nX, m->nY, m->nZ, &xID, &yID, &zID, &timeID); // TODO: does not work when opening an existing file!
+        currentFileID = CreateFile(file_name, m->nX, m->nY, m->nZ, &xID, &yID, &zID, &timeID);
         // add variable Time and pressure:
         nc_def_var(currentFileID, "time", NC_DOUBLE, 1, &timeID, &timeVarID);
         int pressure_dims[4] = {timeID, zID, yID, xID}; // low: why in this order? I guess so it will count in the right order ;)
@@ -88,9 +114,9 @@ int main (int argc , char *argv [])
         size_t start[1], count[1];
         nc_var_par_access(currentFileID, timeVarID, NC_COLLECTIVE);
         find_variable_length(currentFileID, timeVarID, start);
-        D("start writing time at %d\n", start[0]);
+        D("start writing timestep %f into file %s at %d\n", time, file_name, start[0]);
         count[0] = 1;  // writing one value
-        int status = nc_put_vara_double(currentFileID, timeVarID, start, count, time);
+        int status = nc_put_vara_double(currentFileID, timeVarID, start, count, &time);
       }
 
       nc_var_par_access(currentFileID, pressureVarID, NC_COLLECTIVE);
@@ -106,17 +132,13 @@ int main (int argc , char *argv [])
 
     }
 
-    // one file per timestep...
     nc_close(currentFileID);
     D("wrote %s\n", file_name);
 
 
     fca_free(msg);
-
-    // TODO send out new message with new time!
-    // TODO: have iterator to give numbers to file/ create different file names!
   }
 
-
   fca_free(moduleNetCDFWriter);
+  MPI_Finalize();
 }
