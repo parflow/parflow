9e00264b3e44b01ec66d4c3349faeffeccd3d1e3
xy124 <qcd@gmx.net>
2017-10-30 17:28:01 +0100

    Add simplestarter.py

---
+++ flowvr/simplestarter/simplestarter.py
@@ -0,0 +1,49 @@
+import flowvr
+import time
+
+outport = flowvr.OutputPort("out")
+
+stampStartTime = outport.addStamp("stampStartTime", float)
+stampStopTime = outport.addStamp("stampStopTime", float)
+inport = flowvr.InputPort("beginIt")  # really strange that we have to init it here...
+
+v = flowvr.vectorPort()
+v.append(inport)
+v.append(outport)
+
+
+simplestarterModule = flowvr.initModule(v)
+
+
+print ("-------simple starter now waiting!")
+while simplestarterModule.wait() :
+    print ("-------got beginIt")
+    rm = inport.get()
+    rm.clear()
+
+
+    # I do not know how to make an own stamplist atm...
+    m = flowvr.MessageWrite(outport.stamps)
+
+
+    m.setStamp("stampStartTime", 0.0  )
+    m.setStamp("stampStopTime",  0.010)
+
+    # we need to initizlize data also for stamps messages ;)
+    m.data = simplestarterModule.alloc(0)
+    outport.put(m)
+
+    m.clear()
+
+    print ("putting message")
+    time.sleep(1)
+    #break
+
+
+
+print ("-----quit simple starter")
+
+simplestarterModule.close()
+
+
+
