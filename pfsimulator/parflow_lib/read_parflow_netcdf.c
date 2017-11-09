acecc0ff8fee001a515974f556224cb46cdf5c93
Ketan Kulkarni <ketan.kulkarni@gmail.com>
2017-11-09 06:54:40 +0100

    Nc clm input (#45)
    * Additional ParFlow variables output for NetCDF
      Mannings, subsurface data, slopes, mask, DZ multipliers, Overland Sum,
      Evaptrans, evaptrans sum, overland bc flux
    
    * Update user manual for NetCDF variables
    
    * Added NetCDF4 output functionality to CLM. Contains 2D and 3D variables.
      2D variables are defined with x, y and time dimension.
      3D variables are defined with x, y, z and time dimension.
    
    * NetCDF X and Y slopes input can be combined in single file.
    
    * Added a Little Washita reference slope file(x and y slopes).
    
    * Added user manual entries for NetCDF CLM output
    
    * Added NetCDF based forcing for CLM. 
      Little Washita test updated.
    
    * Updated manual entry for CLM Met forcing usage

--- pfsimulator/parflow_lib/read_parflow_netcdf.c
+++ pfsimulator/parflow_lib/read_parflow_netcdf.c
@@ -36,7 +36,7 @@ USA
 
 
 
-void ReadPFNC(char *fileName, Vector *v, char *varName, int tStep)
+void ReadPFNC(char *fileName, Vector *v, char *varName, int tStep, int dimensionality)
 {
 #ifdef PARFLOW_HAVE_NETCDF
   Grid           *grid     = VectorGrid(v);
@@ -52,22 +52,22 @@ void ReadPFNC(char *fileName, Vector *v, char *varName, int tStep)
   double          X, Y, Z;
   int             NX, NY, NZ;
   double          DX, DY, DZ;
-  int 		   ncID, varID;
+  int 		   ncRID, varID;
 
 
-  OpenNCFile(fileName, &ncID);
+  OpenNCFile(fileName, &ncRID);
   ForSubgridI(g, subgrids)
   {
     subgrid   = SubgridArraySubgrid(subgrids, g);
     subvector = VectorSubvector(v, g);
-    ReadNCFile(ncID, varID, subvector,subgrid, varName, tStep);
+    ReadNCFile(ncRID, varID, subvector,subgrid, varName, tStep, dimensionality);
   }
-  nc_close(ncID);
+  nc_close(ncRID);
 
 }
 
 
-void OpenNCFile(char *file_name, int *ncID)
+void OpenNCFile(char *file_name, int *ncRID)
 {
 
   char *switch_name;
@@ -93,22 +93,24 @@ void OpenNCFile(char *file_name, int *ncID)
       sscanf(line,"%s%s", romio_key, value);
       MPI_Info_set(romio_info, romio_key, value);
     }	
-    int res = nc_open_par(file_name,NC_MPIIO, amps_CommWorld, romio_info, ncID);
+    int res = nc_open_par(file_name,NC_MPIIO, amps_CommWorld, romio_info, ncRID);
   }
   else
   {
-    int res = nc_open_par(file_name,NC_MPIIO, amps_CommWorld, MPI_INFO_NULL, ncID);
+    int res = nc_open_par(file_name,NC_MPIIO, amps_CommWorld, MPI_INFO_NULL, ncRID);
   }
 #else
    amps_Printf("Parflow not compiled with NetCDF, can't read NetCDF file\n");
 #endif
 }
 
-void ReadNCFile(int ncID, int varID, Subvector *subvector, Subgrid *subgrid, char *varName, int tStep)
+void ReadNCFile(int ncRID, int varID, Subvector *subvector, Subgrid *subgrid, char *varName, int tStep, int dimensionality)
 {
 #ifdef PARFLOW_HAVE_NETCDF
-  //nc_inq_varid(ncID, "pressure", &varID); 
-  nc_inq_varid(ncID, varName, &varID); 
+  //nc_inq_varid(ncRID, "pressure", &varID); 
+  nc_inq_varid(ncRID, varName, &varID);
+  
+
   int ix = SubgridIX(subgrid);
   int iy = SubgridIY(subgrid);
   int iz = SubgridIZ(subgrid);
@@ -116,7 +118,7 @@ void ReadNCFile(int ncID, int varID, Subvector *subvector, Subgrid *subgrid, cha
   int nx = SubgridNX(subgrid);
   int ny = SubgridNY(subgrid);
   int nz = SubgridNZ(subgrid);
-
+  
   int nx_v = SubvectorNX(subvector);
   int ny_v = SubvectorNY(subvector);
   int nz_v = SubvectorNZ(subvector);
@@ -132,28 +134,53 @@ void ReadNCFile(int ncID, int varID, Subvector *subvector, Subgrid *subgrid, cha
 
   (void)subgrid;
 
-  startp[0] = tStep, countp[0] = 1;
-  startp[1] = iz, countp[1] = nz;
-  startp[2] = iy, countp[2] = ny;
-  startp[3] = ix, countp[3] = nx;
-  
-  char *switch_name;
-  char key[IDB_MAX_KEY_LEN];
-  char *default_val = "None";
-  sprintf(key, "NetCDF.Chunking");
-  switch_name = GetStringDefault(key, "None");
-  if(strcmp(switch_name, default_val) != 0)
+  if (dimensionality == 3)
   {
-    size_t chunksize[4];
-    chunksize[0] = 1;
-    chunksize[1] = GetInt("NetCDF.ChunkZ");
-    chunksize[2] = GetInt("NetCDF.ChunkY");
-    chunksize[3] = GetInt("NetCDF.ChunkX");
-    nc_def_var_chunking(ncID, varID, NC_CHUNKED, chunksize);
-  }
+  nc_inq_varid(ncRID, varName, &varID);
+  
 
-  nc_get_vara_double(ncID, varID, startp, countp, nc_data);
+  int ix = SubgridIX(subgrid);
+  int iy = SubgridIY(subgrid);
+  int iz = SubgridIZ(subgrid);
 
+  int nx = SubgridNX(subgrid);
+  int ny = SubgridNY(subgrid);
+  int nz = SubgridNZ(subgrid);
+  
+  int nx_v = SubvectorNX(subvector);
+  int ny_v = SubvectorNY(subvector);
+  int nz_v = SubvectorNZ(subvector);
+
+  int i, j, k, d, ai;
+
+  size_t startp[4];
+  size_t countp[4];
+
+  double *data;
+  double *nc_data;
+  nc_data = (double *)malloc(sizeof(double)*nx*ny*nz);
+
+  (void)subgrid;
+    startp[0] = tStep, countp[0] = 1;
+    startp[1] = iz, countp[1] = nz;
+    startp[2] = iy, countp[2] = ny;
+    startp[3] = ix, countp[3] = nx;
+
+    char *switch_name;
+    char key[IDB_MAX_KEY_LEN];
+    char *default_val = "None";
+    sprintf(key, "NetCDF.Chunking");
+    switch_name = GetStringDefault(key, "None");
+    if(strcmp(switch_name, default_val) != 0)
+    {
+      size_t chunksize[4];
+      chunksize[0] = 1;
+      chunksize[1] = GetInt("NetCDF.ChunkZ");
+      chunksize[2] = GetInt("NetCDF.ChunkY");
+      chunksize[3] = GetInt("NetCDF.ChunkX");
+      nc_def_var_chunking(ncRID, varID, NC_CHUNKED, chunksize);
+    }
+  nc_get_vara_double(ncRID, varID, startp, countp, nc_data);
   data = SubvectorElt(subvector, ix, iy, iz);
 
   ai = 0, d = 0;
@@ -163,9 +190,66 @@ void ReadNCFile(int ncID, int varID, Subvector *subvector, Subgrid *subgrid, cha
       });
 
   free(nc_data);
+  }
+  else if (dimensionality == 2)
+  {
+  nc_inq_varid(ncRID, varName, &varID);
+  
+
+  int ix = SubgridIX(subgrid);
+  int iy = SubgridIY(subgrid);
+  int iz = SubgridIZ(subgrid);
+
+  int nx = SubgridNX(subgrid);
+  int ny = SubgridNY(subgrid);
+  int nz = SubgridNZ(subgrid);
+  
+  int nx_v = SubvectorNX(subvector);
+  int ny_v = SubvectorNY(subvector);
+  int nz_v = SubvectorNZ(subvector);
+
+  int i, j, k, d, ai;
+
+  size_t startp[3];
+  size_t countp[3];
+
+  double *data;
+  double *nc_data;
+  nc_data = (double *)malloc(sizeof(double)*nx*ny*nz);
+
+  (void)subgrid;
+    startp[0] = tStep, countp[0] = 1;
+    startp[1] = iy, countp[1] = ny;
+    startp[2] = ix, countp[2] = nx;
+
+    char *switch_name;
+    char key[IDB_MAX_KEY_LEN];
+    char *default_val = "None";
+    sprintf(key, "NetCDF.Chunking");
+    switch_name = GetStringDefault(key, "None");
+    if(strcmp(switch_name, default_val) != 0)
+    {
+      size_t chunksize[4];
+      chunksize[0] = 1;
+      chunksize[1] = GetInt("NetCDF.ChunkY");
+      chunksize[2] = GetInt("NetCDF.ChunkX");
+      nc_def_var_chunking(ncRID, varID, NC_CHUNKED, chunksize);
+    }
+
+  nc_get_vara_double(ncRID, varID, startp, countp, nc_data);
+  data = SubvectorElt(subvector, ix, iy, iz);
+
+  ai = 0, d = 0;
+  BoxLoopI1(i, j, k, ix, iy, iz, nx, ny, nz, ai, nx_v, ny_v, nz_v, 1, 1, 1, {
+      data[ai] = nc_data[d];
+      d++;
+      });
+
+  free(nc_data);
+  }
 
 #else
-   amps_Printf("Parflow not compiled with NetCDF, can't read NetCDF file\n");
+  amps_Printf("Parflow not compiled with NetCDF, can't read NetCDF file\n");
 #endif
 
 }
