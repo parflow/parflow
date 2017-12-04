f241f49222785450153f9e120b2d30a97cd7ceaa
xy124 <qcd@gmx.net>
2017-12-04 14:41:41 +0100

    Add hillslope example

---
+++ flowvr/steering-test/do.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export P=2
+export Q=1
+export R=1
+export PROBLEMNAME=hillslope_sens
+export DEBUGFLAGS="-v -v -v"
+export DEBUGFLAGS=""
+../scripts/doMPI.sh
+../scripts/undist.tcl veg_map.pfb
