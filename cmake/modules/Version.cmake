3ba105c03bf381c341d3b9e412e8b01b32de03bd
Steven Smith <smithsg84@gmail.com>
2017-12-16 12:32:33 -0800

    Added support for building documentation to CMake (#60)
    * Added CMake option to build documentation if latex is enabled
    
    * Grab version number from git repo

--- cmake/modules/Version.cmake
+++ cmake/modules/Version.cmake
@@ -2,15 +2,17 @@
 # Macros for helping with version numbers
 
 #
-# Split a version number "X.Y.Z" into seperate variables.
+# Split a version number "vX.Y.Z-tweak-gHASH" into seperate variables.
 # Variable ${packageName}_VERSION must exist and contain the version number.
 #
 macro (version_create_variables packageName)
     if (DEFINED ${packageName}_VERSION)
-        string (REGEX MATCHALL "[0-9]+" _versionComponents "${${packageName}_VERSION}")
+        string (REGEX MATCHALL "[0-9a-zA-Z]+" _versionComponents "${${packageName}_VERSION}")
         list (LENGTH _versionComponents _len)
         if (${_len} GREATER 0)
-            list(GET _versionComponents 0 ${packageName}_VERSION_MAJOR)
+	  list(GET _versionComponents 0 _major)
+	  string (REGEX MATCHALL "[0-9]+" _major ${_major})
+          list(GET _major 0 ${packageName}_VERSION_MAJOR)
         endif()
         if (${_len} GREATER 1)
             list(GET _versionComponents 1 ${packageName}_VERSION_MINOR)
@@ -20,6 +22,11 @@ macro (version_create_variables packageName)
         endif()
         if (${_len} GREATER 3)
             list(GET _versionComponents 3 ${packageName}_VERSION_TWEAK)
+        endif()
+        if (${_len} GREATER 3)
+          list(GET _versionComponents -1 _minor)
+	  string(SUBSTRING ${_minor} 1 -1 _temp)
+	  set(${packageName}_VERSION_HASH ${_temp})
         endif()
         set (${packageName}_VERSION_COUNT ${_len})
     else()
