71ccd50b12d95674ce65b42657fab74ce50fc450
xy124 <qcd@gmx.net>
2018-01-22 16:43:25 +0100

    Cleanup

--- flowvr/parflowvr/do.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export P=2
-export Q=1
-export R=1
-export PROBLEMNAME=fvr
-export DEBUGFLAGS="-v -v -v"
-export DEBUGFLAGS=""
-../scripts/doMPI.sh
--- flowvr/parflowvr/watch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-when-changed -r -s $PARFLOW_DIR/ -c bash -c "killall do.sh ; ./do.sh & exit 0"
