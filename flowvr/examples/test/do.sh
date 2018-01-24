90b71c078a3105f406b4bbc95abaa686372aeda3
xy124 <qcd@gmx.net>
2018-01-24 10:25:00 +0100

    Add test to example

--- flowvr/examples/test/do.sh
+++ flowvr/examples/test/do.sh
@@ -5,4 +5,20 @@ export Q=1
 export R=1
 export PROBLEMNAME=default_richards
 export DEBUGFLAGS=""
+
+# Python as reference:
+export ANALYZER=../Python-analyzer-template.py
 $PARFLOW_DIR/bin/parflowvr/doMPI.sh
+
+rm -rf correct_output
+mkdir correct_output
+mv *.pfb correct_output
+
+# Compare with C run!
+cd ..
+gcc -L$PARFLOW_DIR/lib -I../pfanalyzer -I/usr/local/include/fca C-analyzer-template.c -lfca -lpfanalyzer
+cd test
+export ANALYZER=../a.out
+$PARFLOW_DIR/bin/parflowvr/doMPI.sh
+
+tclsh test.tcl
