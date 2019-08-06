6617efaa217da477d45a91cc87398b4ca59e589c
Laura Condon <lecondon@email.arizona.edu>
2019-08-06 14:38:13 -0700

    Removing LLNL copyright statement

--- pfsimulator/parflow_lib/overlandflow_eval_Kin.c
+++ pfsimulator/parflow_lib/overlandflow_eval_Kin.c
@@ -1,16 +1,4 @@
 /*BHEADER*********************************************************************
- *
- *  Copyright (c) 1995-2009, Lawrence Livermore National Security,
- *  LLC. Produced at the Lawrence Livermore National Laboratory. Written
- *  by the Parflow Team (see the CONTRIBUTORS file)
- *  <parflow@lists.llnl.gov> CODE-OCEC-08-103. All rights reserved.
- *
- *  This file is part of Parflow. For details, see
- *  http://www.llnl.gov/casc/parflow
- *
- *  Please read the COPYRIGHT file or Our Notice and the LICENSE file
- *  for the GNU Lesser General Public License.
- *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License (as published
  *  by the Free Software Foundation) version 2.1 dated February 1999.
