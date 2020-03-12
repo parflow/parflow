11ef866480abb2bf97f6fd7fcfaa21c0af73c610
Mathieu Lobet <mathieu.lobet@cea.fr>
2020-03-12 15:01:19 +0100

    PDI integration - add pdi to cmake

---
+++ cmake/modules/FindPDI.cmake
@@ -0,0 +1,21 @@
+include(FindPackageHandleStandardArgs)
+
+if(NOT PDI_ROOT)
+    set(PDI_ROOT $ENV{PDI_ROOT})
+endif()
+
+find_path(PDI_INCLUDE_DIR NAMES pdi.h
+  PATH_SUFFIXES pdi
+  HINTS ${PDI_ROOT}/include
+  PATHS /usr/include /usr/local/include)
+  
+find_library(PDI_LIBRARY NAMES pdi yaml paraconf
+  HINTS ${PDI_ROOT}/lib
+  PATHS /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib)
+  
+set(PDI_INCLUDE_DIRS ${PDI_INCLUDE_DIR})
+set(PDI_LIBRARIES ${PDI_LIBRARY})
+
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PDI DEFAULT_MSG PDI_LIBRARIES PDI_INCLUDE_DIRS)
+
+MARK_AS_ADVANCED(PDI_INCLUDE_DIRS PDI_LIBRARIES)
---
+++ pfsimulator/parflow_lib/write_parflow_pdi.c
@@ -0,0 +1,44 @@
+/*BHEADER*********************************************************************
+ *
+ *  This file is part of Parflow. For details, see
+ *
+ *  Please read the COPYRIGHT file or Our Notice and the LICENSE file
+ *  for the GNU Lesser General Public License.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License (as published
+ *  by the Free Software Foundation) version 2.1 dated February 1999.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms
+ *  and conditions of the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ *  USA
+ **********************************************************************EHEADER*/
+/*****************************************************************************
+*
+* Routines to write a Vector to a file in full or scattered form.
+*
+*****************************************************************************/
+
+#include "parflow.h"
+#ifdef PARFLOW_HAVE_PDI
+#include <pdi.h>
+#endif
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+
+#ifdef PARFLOW_HAVE_PDI
+static bool is2Ddefined = false;
+static bool is3Ddefined = false;
+static bool isTdefined = false;
+#endif
