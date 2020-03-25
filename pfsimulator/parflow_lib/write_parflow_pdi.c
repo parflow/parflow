84530a4d1fabeec15ce2ccb49b84b50c288167ce
Mathieu Lobet <mathieu.lobet@cea.fr>
2020-03-25 17:53:08 +0100

    Prepapration of the pointer version

--- pfsimulator/parflow_lib/write_parflow_pdi.c
+++ pfsimulator/parflow_lib/write_parflow_pdi.c
@@ -94,20 +94,138 @@ void     WritePDI(
   
   if (p == 0)
   {
+      
+    const char pdi_conf[] = "\
+.types:\n\
+  - &Subregion\n\
+    type: record\n\
+    buffersize: 56\n\
+    members:\n\
+      ix:      {disp: 0, type: int}\n\
+      iy:      {disp: 4, type: int}\n\
+      iz:      {disp: 8, type: int}\n\
+      nx:      {disp: 12, type: int}\n\
+      ny:      {disp: 16, type: int}\n\
+      nz:      {disp: 20, type: int}\n\
+      sx:      {disp: 24, type: int}\n\
+      sy:      {disp: 28, type: int}\n\
+      sz:      {disp: 32, type: int}\n\
+      rx:      {disp: 36, type: int}\n\
+      ry:      {disp: 40, type: int}\n\
+      rz:      {disp: 44, type: int}\n\
+      level:   {disp: 48, type: int}\n\
+      process: {disp: 52, type: int}\n\
+  - &Subvector\n\
+    type: \n\
+    buffersize: 32\n\
+    members:\n\
+      data:\n\
+        disp: 0\n\
+        type: pointer\n\
+        subtype:\n\
+          type: array\n\
+          size: $temp_data_size\n\
+          subtype: double\n\
+      allocated:\n\
+        disp: 8\n\
+        type: int\n\
+      data_space:\n\
+        disp: 16\n\
+        type: pointer\n\
+        subtype: *Subregion\n\
+      data_size:\n\
+        disp: 24\n\
+        type: int\n\
+  - &SubregionArray\n\
+    type: record\n\
+    buffersize: 16\n\
+    members:\n\
+      subregions:\n\
+        disp: 0\n\
+        type: pointer\n\
+        subtype:\n\
+          type: array\n\
+          size: $temp_num_grid\n\
+          subtype:\n\
+            type: pointer\n\
+            subtype: *Subregion\n\
+      size:\n\
+        disp: 8\n\
+        type: int\n\
+  - &Grid\n\
+    type: record\n\
+    buffersize: 32\n\
+    members:\n\
+      subgrids:\n\
+        disp: 0\n\
+        type: pointer\n\
+        subtype:\n\
+          type: array\n\
+          size: 1\n\
+          subtype: *SubregionArray\n\
+      all_subgrids:\n\
+        disp: 8\n\
+        type: pointer\n\
+        subtype:\n\
+          type: array\n\
+          size: 1\n\
+          subtype: *SubregionArray\n\
+      neighbors:\n\
+        disp: 16\n\
+        type: pointer\n\
+        subtype:\n\
+          type: array\n\
+          size: 1\n\
+          subtype: *SubregionArray\n\
+      size:\n\
+        disp: 24\n\
+        type: int\n\
+logging: trace\n\
+metadata:\n\
+  it: int\n\
+  temp_data_size: int\n\
+  temp_num_grid: int\n\
+  postfix:  { type: array, subtype: char, size: 32 }\n\
+  X: double\n\
+  Y: double\n\
+  Z: double\n\
+  NX: double\n\
+  NY: double\n\
+  NZ: double\n\
+  num_subgrids: int\n\
+data:\n\
+  vector_data:\n\
+    type: record\n\
+    buffersize: 8\n\
+    members:\n\
+      subvectors:\n\
+        disp: 0\n\
+        type: pointer\n\
+        subtype:\n\
+          type: array\n\
+          size: 8\n\
+          subtype:\n\
+            type: pointer\n\
+            subtype: *Subvector\n\
+";
+  //     data_size:\n\
+  //       disp: 8\n\
+  //       type: int\n\
+  //     grid:\n\
+  //       disp: 16\n\
+  //       type: pointer\n\
+  //       subtype: *Grid\n\
+  //     data_space:\n\
+  //       disp: 24\n\
+  //       type: pointer\n\
+  //       subtype: *SubregionArray\n\
+  //     size:\n\
+  //       disp: 32\n\
+  //       type: int\n\
+//";
     FILE *fp;
     fp = fopen("conf.yml", "w");
-    fprintf(fp, "pdi:\n");
-    fprintf(fp, "  metadata:\n");
-    fprintf(fp, "    it: int\n");
-    fprintf(fp, "    postfix:  { type: array, subtype: char, size: 32 }\n");
-    fprintf(fp, "    X: double\n");
-    fprintf(fp, "    Y: double\n");
-    fprintf(fp, "    Z: double\n");
-    fprintf(fp, "    NX: int\n");
-    fprintf(fp, "    NY: int\n");
-    fprintf(fp, "    NZ: int\n");
-    fprintf(fp, "    num_subgrids: int\n");
-    fprintf(fp, "  data:\n");
+    fprintf(fp, pdi_conf);
     // fprintf(fp, "    subvector:\n");
     // fprintf(fp, "    type: record\n");
     // fprintf(fp, "    buffersize: 4\n");
@@ -115,41 +233,32 @@ void     WritePDI(
     // fprintf(fp, "      size:\n");
     // fprintf(fp, "        disp: 0\n");
     // fprintf(fp, "        type: int\n");
-    fprintf(fp, "  plugins:   \n");
-    fprintf(fp, "    mpi:   \n");
-    fprintf(fp, "    decl_hdf5:   \n");
-    fprintf(fp, "    - file: test.${it}.h5   \n");
-    fprintf(fp, "      communicator: $MPI_COMM_WORLD   \n");
-    fprintf(fp, "      write: [ X, Y, Z, NX, NY, NZ, num_subgrids ]   \n");
+    fprintf(fp, "plugins:   \n");
+    fprintf(fp, "  mpi:   \n");
+    fprintf(fp, "  decl_hdf5:   \n");
+    fprintf(fp, "  - file: test.${it}.h5   \n");
+    fprintf(fp, "    communicator: $MPI_COMM_WORLD   \n");
+    fprintf(fp, "    write: [ X, Y, Z, NX, NY, NZ, num_subgrids ]   \n");
     fprintf(fp, "          \n");
     fclose(fp);
     
     // load the configuration tree
     PC_tree_t conf = PC_parse_path("conf.yml");
-    PDI_init(PC_get(conf, ".pdi"));
+    PDI_init(conf);
     
-    PDI_share("it", &iteration, PDI_OUT);
-    PDI_reclaim("it");
+    PDI_expose("it", &iteration, PDI_OUT);
     
-    PDI_share("postfix", &filename, PDI_OUT);
-    PDI_reclaim("postfix");
+    PDI_expose("postfix", &filename, PDI_OUT);
     
-    PDI_share("X", &BackgroundX(GlobalsBackground), PDI_OUT);
-    PDI_reclaim("X");
-    PDI_share("Y", &BackgroundY(GlobalsBackground), PDI_OUT);
-    PDI_reclaim("Y");
-    PDI_share("Z", &BackgroundY(GlobalsBackground), PDI_OUT);
-    PDI_reclaim("Z");
+    PDI_expose("X", &BackgroundX(GlobalsBackground), PDI_OUT);
+    PDI_expose("Y", &BackgroundY(GlobalsBackground), PDI_OUT);
+    PDI_expose("Z", &BackgroundY(GlobalsBackground), PDI_OUT);
     
-    PDI_share("NX", &SubgridNX(GridBackground(grid)), PDI_OUT);
-    PDI_reclaim("NX");
-    PDI_share("NY", &SubgridNY(GridBackground(grid)), PDI_OUT);
-    PDI_reclaim("NY");
-    PDI_share("NZ", &SubgridNZ(GridBackground(grid)), PDI_OUT);
-    PDI_reclaim("NZ");
+    PDI_expose("NX", &SubgridNX(GridBackground(grid)), PDI_OUT);
+    PDI_expose("NY", &SubgridNY(GridBackground(grid)), PDI_OUT);
+    PDI_expose("NZ", &SubgridNZ(GridBackground(grid)), PDI_OUT);
     
-    PDI_share("num_subgrids", &num_subgrids, PDI_OUT);
-    PDI_reclaim("num_subgrids");
+    PDI_expose("num_subgrids", &num_subgrids, PDI_OUT);
     
     // PDI_share("NX", &SubgridNX(GridBackground(grid)), PDI_OUT);
     // PDI_reclaim("NX");
