aa1b5775879aa59e0fd659b7812aac7a16ff618b
Jaro Hokkanen <j.hokkanen@fz-juelich.de>
2020-04-22 09:36:09 +0200

    Move PF_COMP_UNIT_TYPE definitions to CMakeLists.txt

--- pfsimulator/parflow_lib/axpy.cpp
+++ pfsimulator/parflow_lib/axpy.cpp
@@ -1,4 +1,4 @@
-/* PF_COMP_UNIT_TYPE determines the behavior of the NVCC compilation unit and/or OpenMP loops:
+/* PF_COMP_UNIT_TYPE determines the behavior of the accelerated compilation unit
 	 ------------------------------------------------------------
    CUDA
 	 ------------------------------------------------------------
@@ -6,18 +6,18 @@
 	 2:     NVCC compiler, Unified Memory allocation, Sequential loops on host
 	 Other: NVCC compiler, Standard heap allocation, Sequential loops on host
 
-
 	 ------------------------------------------------------------
    OpenMP
 	 ------------------------------------------------------------
-   1:     CXX compiler, Unified Memory allocation, Parallel loops on CPU
+     1:     CXX compiler, Unified Memory allocation, Parallel loops on CPU
 	 2:     CXX compiler, Unified Memory allocation, Sequential loops on CPU
 	 Other: CXX compiler, Standard heap allocation, Sequential loops on CPU
 */
-#define PF_COMP_UNIT_TYPE 1
 
-/* extern "C" is required for the C source files when compiled with NVCC */
+// This definition is now controlled by CMake (./CMakeLists.txt)
+// #define PF_COMP_UNIT_TYPE 1
 
+/* extern "C" is required for the C source files when compiled with CPP compiler */
 extern "C"{
   #include "axpy.c"
 }
