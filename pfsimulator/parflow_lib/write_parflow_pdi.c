bd25bb303f24fadcf13e22d047a70c0ec37a91bd
Mathieu Lobet <mathieu.lobet@cea.fr>
2020-04-26 16:21:44 +0200

    - ParFlow does not create the conf.yml anymore - conf.yml located in /pdi - Update of write_parflow_pdi to include dynamic parameters

--- pfsimulator/parflow_lib/write_parflow_pdi.c
+++ pfsimulator/parflow_lib/write_parflow_pdi.c
@@ -60,23 +60,10 @@ void     WritePDI(
 
   long size;
   
-  BeginTiming(PFBTimingIndex);
+  //BeginTiming(PFBTimingIndex);
   
+  /* Current rank */
   p = amps_Rank(amps_CommWorld);
-
-  if (p == 0)
-    size = 6 * amps_SizeofDouble + 4 * amps_SizeofInt;
-  else
-    size = 0;
-  
-  /* Compute total size */
-  ForSubgridI(g, subgrids)
-  {
-    subgrid = SubgridArraySubgrid(subgrids, g);
-    subvector = VectorSubvector(v, g);
-
-    size += SizeofPFBinarySubvector(subvector, subgrid);
-  }
   
   /* Compute number of patches to write */
   int num_subgrids = GridNumSubgrids(grid);
@@ -88,177 +75,37 @@ void     WritePDI(
     amps_FreeInvoice(invoice);
   }
   
+  // File name to be passed to PDI
   char filename[32] = "";
-  /* open file */
   sprintf(filename, "%s.%s", file_prefix, file_suffix);
   
   if (p == 0)
   {
-    Vector vv;
+    /* Size of the Vector structure for PDI */
+    int vector_pdi_buffer_size = sizeof(Vector);
+    /* disp set dynamically for PDI due to SHMEM_OBJECTS */
+    int grid_disp       = ((char*)&(v->grid)) - ((char*)v);
+    int data_space_disp = ((char*)&(v->data_space)) - ((char*)v);
+    int size_disp       = ((char*)&(v->size)) - ((char*)v);
+      
     printf("Size of Vector: %ld\n", sizeof(Vector));
     printf("Size of Grid: %ld\n", sizeof(Grid));
-    printf("Data size until **subvectors: %ld\n", ((char*)&v)-((char*)&v->subvectors[0][0]));
-    printf("Data size until data_size: %ld\n", ((char*)&vv)-((char*)&vv.data_size));
-    printf("Data size until size: %ld\n", ((char*)&vv)-((char*)&vv.size));
+    printf("Data size until **subvectors: %ld\n", ((char*)&(v->subvectors)) - (char*)v);
+    printf("Data size until data_size: %ld\n", ((char*)&(v->data_size)) - ((char*)v));
+    printf("Data size until grid: %d\n", grid_disp);
+    printf("Data size until grid: %d\n", data_space_disp);
+    printf("Data size until size: %d\n", size_disp);
     // printf("Data size until data_size: %ld\n", ((char*)&vv)-((char*)&vv.data_size));
     // ((char*)&a_struct)-((char*)&a_struct.grid)
-    
-    
-    
-    const char pdi_conf[] = "\
-.types:\n\
-  - &ComputePkg\n\
-    type: record\n\
-  - &Subregion\n\
-    type: record\n\
-    buffersize: 56\n\
-    members:\n\
-      ix:      {disp: 0, type: int}\n\
-      iy:      {disp: 4, type: int}\n\
-      iz:      {disp: 8, type: int}\n\
-      nx:      {disp: 12, type: int}\n\
-      ny:      {disp: 16, type: int}\n\
-      nz:      {disp: 20, type: int}\n\
-      sx:      {disp: 24, type: int}\n\
-      sy:      {disp: 28, type: int}\n\
-      sz:      {disp: 32, type: int}\n\
-      rx:      {disp: 36, type: int}\n\
-      ry:      {disp: 40, type: int}\n\
-      rz:      {disp: 44, type: int}\n\
-      level:   {disp: 48, type: int}\n\
-      process: {disp: 52, type: int}\n\
-  - &Subvector\n\
-    type: record \n\
-    buffersize: 32\n\
-    members:\n\
-      data:\n\
-        disp: 0\n\
-        type: pointer\n\
-        subtype:\n\
-          type: array\n\
-          size: $temp_data_size\n\
-          subtype: double\n\
-      allocated:\n\
-        disp: 8\n\
-        type: int\n\
-      data_space:\n\
-        disp: 16\n\
-        type: pointer\n\
-        subtype: *Subregion\n\
-      data_size:\n\
-        disp: 24\n\
-        type: int\n\
-  - &SubregionArray\n\
-    type: record\n\
-    buffersize: 16\n\
-    members:\n\
-      subregions:\n\
-        disp: 0\n\
-        type: pointer\n\
-        subtype:\n\
-          type: array\n\
-          size: $temp_num_grid\n\
-          subtype:\n\
-            type: pointer\n\
-            subtype: *Subregion\n\
-      size:\n\
-        disp: 8\n\
-        type: int\n\
-  - &Grid\n\
-    type: record\n\
-    buffersize: 40\n\
-    members:\n\
-      subgrids:\n\
-        disp: 0\n\
-        type: pointer\n\
-        subtype:\n\
-          type: array\n\
-          size: 1\n\
-          subtype: *SubregionArray\n\
-      all_subgrids:\n\
-        disp: 8\n\
-        type: pointer\n\
-        subtype:\n\
-          type: array\n\
-          size: 1\n\
-          subtype: *SubregionArray\n\
-      size:\n\
-        disp: 16\n\
-        type: int\n\
-logging: trace\n\
-metadata:\n\
-  it: int\n\
-  temp_data_size: int\n\
-  temp_num_grid: int\n\
-  postfix:  { type: array, subtype: char, size: 32 }\n\
-  X: double\n\
-  Y: double\n\
-  Z: double\n\
-  NX: double\n\
-  NY: double\n\
-  NZ: double\n\
-  num_subgrids: int\n\
-data:\n\
-  vector_data:\n\
-    type: record\n\
-    buffersize: 128\n\
-    members:\n\
-      subvectors:\n\
-        disp: 0\n\
-        type: pointer\n\
-        subtype:\n\
-          type: array\n\
-          size: 1\n\
-          subtype:\n\
-            type: pointer\n\
-            subtype: *Subvector\n\
-      data_size:\n\
-        disp: 8\n\
-        type: int\n\
-";
-// compute_pkgs:\n\
-//   disp: 16\n\
-//   type: pointer\n\
-//   subtype:\n\
-//     type: array\n\
-//     size: 1\n\
-//     subtype: *ComputePkg\n\
-// background:\n
-FILE *fp;
-fp = fopen("conf.yml", "w");
-fprintf(fp, pdi_conf);
-#ifdef SHMEM_OBJECTS
-  fprintf(fp,"      int: shmem_offset\n");
-#endif
-const char pdi_conf_2[] = "\
-      grid:\n\
-        disp: 16\n\
-        type: pointer\n\
-        subtype: *Grid\n\
-      data_space:\n\
-        disp: 24\n\
-        type: pointer\n\
-        subtype: *SubregionArray\n\
-      size:\n\
-        disp: 32\n\
-        type: int\n\
-plugins:   \n\
-  decl_hdf5:   \n\
-  - file: test.${it}.h5   \n\
-    write: [ X, Y, Z, NX, NY, NZ, num_subgrids, temp_data_size, temp_num_grid, vector_data ]   \n\
-";
-    fprintf(fp, pdi_conf_2);
-    //fprintf(fp, "  mpi:   \n");
-    //fprintf(fp, "    communicator: $MPI_COMM_WORLD   \n");
-    fclose(fp);
     
     // load the configuration tree
     PC_tree_t conf = PC_parse_path("conf.yml");
     PDI_init(conf);
     
     PDI_expose("it", &iteration, PDI_OUT);
+    PDI_expose("parflowrank", &p, PDI_OUT);
     
-    PDI_expose("postfix", &filename, PDI_OUT);
+    PDI_expose("filename", &filename, PDI_OUT);
     
     PDI_expose("X", &BackgroundX(GlobalsBackground), PDI_OUT);
     PDI_expose("Y", &BackgroundY(GlobalsBackground), PDI_OUT);
@@ -268,28 +115,30 @@ plugins:   \n\
     PDI_expose("NY", &SubgridNY(GridBackground(grid)), PDI_OUT);
     PDI_expose("NZ", &SubgridNZ(GridBackground(grid)), PDI_OUT);
     
+    PDI_expose("DX", &BackgroundDX(GlobalsBackground), PDI_OUT);
+    PDI_expose("DY", &BackgroundDY(GlobalsBackground), PDI_OUT);
+    PDI_expose("DZ", &BackgroundDZ(GlobalsBackground), PDI_OUT);
+    
     PDI_expose("num_subgrids", &num_subgrids, PDI_OUT);
     
     subvector = VectorSubvector(v, 0);
     int temp_data_size = SubvectorDataSize(subvector);
     PDI_expose("temp_data_size", &temp_data_size, PDI_OUT);
     
-    int temp_num_grid = 1;
-    PDI_expose("temp_num_grid", &temp_num_grid, PDI_OUT);
+    int num_grid = 1;
+    PDI_expose("num_grid", &num_grid, PDI_OUT);
+    
+    PDI_expose("vector_pdi_buffer_size", &vector_pdi_buffer_size, PDI_OUT);
+    PDI_expose("grid_disp",       &grid_disp, PDI_OUT);
+    PDI_expose("data_space_disp", &data_space_disp, PDI_OUT);
+    PDI_expose("size_disp",       &size_disp, PDI_OUT);
     
     PDI_expose("vector_data", v, PDI_OUT);
-    
-    // PDI_share("NX", &SubgridNX(GridBackground(grid)), PDI_OUT);
-    // PDI_reclaim("NX");
-    // PDI_share("NY", &SubgridNY(GridBackground(grid)), PDI_OUT);
-    // PDI_reclaim("NY");
-    // PDI_share("NZ", &SubgridNZ(GridBackground(grid)), PDI_OUT);
-    // PDI_reclaim("NZ");
     
     // finalize PDI
     PDI_finalize();
   }
   
-  EndTiming(PFBTimingIndex);
+  //EndTiming(PFBTimingIndex);
   
 }
