6552abeac88b47cdf9975c7d485334c71ccc1225
Steven Smith <smith84@llnl.gov>
2020-07-24 17:39:38 -0700

    Rename PARFLOW_TEST_FORCE_MPIEXEC flag and added to cache with description

--- cmake/modules/RunAmpsTest.cmake
+++ cmake/modules/RunAmpsTest.cmake
@@ -6,6 +6,7 @@
 cmake_minimum_required(VERSION 3.14)
 
 # Execute command with error check
+# A rank of -1 is used for a sequential run that is run without mpiexec.
 # all parameters passed in as reference
 macro(pf_amps_exec_check cmd ranks args)
 
