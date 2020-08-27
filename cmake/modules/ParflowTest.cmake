c38620b03b59b5a5702fc8bdb6ec8471ea3b7e90
Garrett Rapp <grapp@email.arizona.edu>
2020-08-27 11:47:52 -0700

    adding parallel python tests

--- cmake/modules/ParflowTest.cmake
+++ cmake/modules/ParflowTest.cmake
@@ -117,3 +117,19 @@ function (pf_add_py_test test_name)
   endif()
 
 endfunction()
+
+# Add parflow testing in parallel of a single Python file
+function (pf_add_py_parallel_test test_name topology_P topology_Q topology_R)
+
+  if(IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${test_name}")
+    add_test(
+      NAME "py_${test_name}_${topology_P}_${topology_Q}_${topology_R}"
+      COMMAND ${PARFLOW_PYTHON} "${CMAKE_CURRENT_SOURCE_DIR}/${test_name}/${test_name}.py -p ${topology_P} -q ${topology_Q} -r ${topology_R}"
+    )
+    set_tests_properties(
+      "py_${test_name}"
+      PROPERTIES ENVIRONMENT PYTHONPATH=${PROJECT_SOURCE_DIR}/pftools/python:$ENV{PYTHONPATH}
+    )
+  endif()
+
+endfunction()
