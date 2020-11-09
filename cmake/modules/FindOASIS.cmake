e9aceb8b4948dc2e9892bfb50b91ffb973d76f84
Jaro Hokkanen <j.hokkanen@fz-juelich.de>
2020-11-09 18:32:37 +0100

    Remove hardcoded lib file names

--- cmake/modules/FindOASIS.cmake
+++ cmake/modules/FindOASIS.cmake
@@ -1,14 +1,14 @@
 include(FindPackageHandleStandardArgs)
 
 #OAS3 DEPENDENCIES
-find_path(MCT_Fortran_LIBDIR libmct.a HINTS ${OAS3_ROOT}/lib)
-find_path(MPEU_Fortran_LIBDIR libmpeu.a HINTS ${OAS3_ROOT}/lib)
-find_path(SCRIP_Fortran_LIBDIR libscrip.a HINTS ${OAS3_ROOT}/lib)
+find_library(MCT_Fortran_LIBRARY mct HINTS ${OAS3_ROOT}/lib)
+find_library(MPEU_Fortran_LIBRARY mpeu HINTS ${OAS3_ROOT}/lib)
+find_library(SCRIP_Fortran_LIBRARY scrip HINTS ${OAS3_ROOT}/lib)
 
 #OAS3 LIB
-if(MCT_Fortran_LIBDIR AND MPEU_Fortran_LIBDIR AND SCRIP_Fortran_LIBDIR)
+if(MCT_Fortran_LIBRARY AND MPEU_Fortran_LIBRARY AND SCRIP_Fortran_LIBRARY)
   find_path(OASIS_Fortran_INCLUDES mod_oasis.mod HINTS ${OAS3_ROOT}/build/lib/psmile.MPI1)
-  find_path(OASIS_Fortran_LIBDIR libpsmile.MPI1.a HINTS ${OAS3_ROOT}/lib)
-endif(MCT_Fortran_LIBDIR AND MPEU_Fortran_LIBDIR AND SCRIP_Fortran_LIBDIR)
+  find_library(OASIS_Fortran_LIBRARY psmile.MPI1 HINTS ${OAS3_ROOT}/lib)
+endif(MCT_Fortran_LIBRARY AND MPEU_Fortran_LIBRARY AND SCRIP_Fortran_LIBRARY)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(OASIS DEFAULT_MSG OASIS_Fortran_LIBDIR OASIS_Fortran_INCLUDES)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(OASIS DEFAULT_MSG OASIS_Fortran_LIBRARY OASIS_Fortran_INCLUDES)
