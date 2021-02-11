09c5a632ffd18512a961bff36b66f024a22b82b4
Patrick Avery <patrick.avery@kitware.com>
2021-02-11 16:03:43 -0600

    Bump pftools python to 1.1.2
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- pftools/python/setup.py
+++ pftools/python/setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='pftools',
-    version="1.1.1",
+    version="1.1.2",
     description=('A Python package creating an interface with the ParFlow '
                  'hydrologic model.'),
     long_description=README,
