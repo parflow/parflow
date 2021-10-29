c7d6b2233726eea0a3eb034f661d64df2d6c1a0f
kvrigor <kvrigor@users.noreply.github.com>
2021-10-29 08:49:51 +0200

    Supported OASIS3-MCT v4.0 API usage (initial changes)

--- pfsimulator/amps/oas3/CMakeLists.cmake
+++ pfsimulator/amps/oas3/CMakeLists.cmake
@@ -17,15 +17,24 @@ set(AMPS_SRC_FILES
   amps_test.c
   amps_unpack.c
   amps_vector.c
-  oas_pfl_define.F90
-  oas_pfl_finalize.F90
-  oas_pfl_init.F90
-  oas_pfl_rcv.F90
-  oas_pfl_snd.F90
-  oas_pfl_vardef.F90
-  receive_fld2_clm.F90
-  send_fld2_clm.F90
   )
+
+if (${OAS3_API_VERSION} STREQUAL "2.0")
+  message(STATUS "Using legacy OASIS3-MCT API")
+  list(APPEND AMPS_SRC_FILES oas_pfl_define.F90
+                             oas_pfl_finalize.F90
+                             oas_pfl_init.F90
+                             oas_pfl_rcv.F90
+                             oas_pfl_snd.F90
+                             oas_pfl_vardef.F90
+                             receive_fld2_clm.F90
+                             send_fld2_clm.F90)
+elseif (${OAS3_API_VERSION} STREQUAL "4.0")
+  message(STATUS "Using OASIS3-MCT 4.0 APIs")
+  list(APPEND AMPS_SRC_FILES oas_pfl_mod.F90)
+else()
+  message(FATAL_ERROR "OAS3_API_VERSION=${OAS3_API_VERSION} is unsupported.")
+endif(${OAS3_API_VERSION})
 
 if((${PARFLOW_HAVE_CUDA}) AND (NOT (${PARFLOW_HAVE_KOKKOS})))
   list(APPEND AMPS_SRC_FILES amps_gpupacking.cu)
