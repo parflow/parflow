5837b12d9813b028b23a5aabb8d6011b85d1e7ba
Calla Chennault <callachennault@gmail.com>
2022-03-14 20:16:34 -0400

    Swap x and y in vegm_array and update PFTools verison number (#372)
    The _read_vegm function in PFTools is inconsistent with parflow-python xy indexing. This fix swaps the indices so that the x index of the vegm_array correctly reflects the columns and y index reflects the rows.

--- pftools/python/setup.py
+++ pftools/python/setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='pftools',
-    version="1.3.2",
+    version="1.3.3",
     description=('A Python package creating an interface with the ParFlow '
                  'hydrologic model.'),
     long_description=README,
