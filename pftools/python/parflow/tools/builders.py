30fb20f29ae0541516001aded9d2c3aeff7d7f6c
westb2 <west.bd@gmail.com>
2022-04-06 16:31:32 -0700

    fixed pftools build in docker dev environment

--- pftools/python/parflow/tools/builders.py
+++ pftools/python/parflow/tools/builders.py
@@ -801,6 +801,32 @@ class SubsurfacePropertiesBuilder(TableToProperties):
         return 'subsurface_'
 
 
+class WellsPropertiesBuilder(TableToProperties):
+
+    def __init__(self, run=None):
+        super().__init__(run)
+
+    @property
+    def reference_file(self):
+        return 'ref/table_keys.yaml'
+
+    @property
+    def key_root(self):
+        return self.run.Wells
+
+    @property
+    def unit_string(self):
+        return 'Wells'
+
+    @property
+    def default_db(self):
+        return 'conus_1'
+
+    @property
+    def db_prefix(self):
+        return 'wells_'
+
+
 # -----------------------------------------------------------------------------
 # Vegetation parameter property input helper
 # -----------------------------------------------------------------------------
---
+++ test/python/WellsBuilder.py
@@ -0,0 +1,27 @@
+from parflow.tools.builders import TableToProperties
+
+
+class WellsPropertiesBuilder(TableToProperties):
+
+    def __init__(self, run=None):
+        super().__init__(run)
+
+    @property
+    def reference_file(self):
+        return '/data/parflow/pftools/python/parflow/tools/ref/wells_keys.yaml'
+
+    @property
+    def key_root(self):
+        return self.run.Wells
+
+    @property
+    def unit_string(self):
+        return 'Wells'
+
+    @property
+    def default_db(self):
+        return 'conus_1'
+
+    @property
+    def db_prefix(self):
+        return 'wells_'
