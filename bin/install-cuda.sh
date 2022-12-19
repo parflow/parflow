da2567dc5b1e396fcf807a17700fa69168b84dac
Steven Smith <smith84@llnl.gov>
2022-12-19 13:23:20 -0800

    Move script and add author / license

--- bin/install-cuda.sh
+++ bin/install-cuda.sh
@@ -1,11 +1,36 @@
-# @todo - better / more robust parsing of inputs from env vars.
+# Script adopted from https://github.com/ptheywood/cuda-cmake-github-actions
+# Copyright (c) 2021 Peter Heywood
+# Released under MIT license
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+#
+# Script assumes environment variables are set:
+#    CUDA_VERSION_MAJOR_MINOR is set to CUDA version to install (e.g. 11.8.0)
+#    CUDA_OS is set to Nvidia labeling for OS (e.g. ubuntu2204)
+#
+
 ## -------------------
 ## Constants
 ## -------------------
 
 # List of sub-packages to install.
-# @todo - pass this in from outside the script? 
-# @todo - check the specified subpackages exist via apt pre-install?  apt-rdepends cuda-9-0 | grep "^cuda-"?
 
 # Ideally choose from the list of meta-packages to minimise variance between cuda versions (although it does change too). Some of these packages may not be availble in older CUDA releases
 CUDA_PACKAGES_IN=(
