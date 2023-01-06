8d8623ef39ab42d782f9a5378f34e96225c7d335
Steven Smith <smith84@llnl.gov>
2023-01-06 09:13:52 -0800

    Update version number

--- docs/user_manual/conf.py
+++ docs/user_manual/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ParFlow'
-copyright = u'2022'
+copyright = u'2023'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
