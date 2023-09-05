d456710df22356e175711b321f8952eb48e198c2
George Artavanis <ga6@verde.princeton.edu>
2023-09-05 10:59:25 -0400

    added richards_FBy test

--- test/python/richards_FBy.py
+++ test/python/richards_FBy.py
@@ -3,19 +3,22 @@
 # with simple flow domains, like a wall or a fault.
 #------------------------------------------------------------------
 
+import sys
 from parflow import Run, write_pfb
 from parflow.tools.fs import get_absolute_path, mkdir, chdir
+from parflow.tools.compare import pf_test_file
 import numpy as np
 
-rich_fby = Run("richards_FBy", __file__)
+run_name = "richards_FBy"
+rich_fby = Run(run_name, __file__)
 
 #---------------------------------------------------------
 # Creating and navigating to output directory
 #---------------------------------------------------------
 
-dir_name = get_absolute_path('test_output/rich_fby')
-mkdir(dir_name)
-chdir(dir_name)
+correct_output_dir_name = get_absolute_path('../correct_output')
+new_output_dir_name = get_absolute_path('test_output/richards_fby')
+mkdir(new_output_dir_name)
 
 #------------------------------------------------------------------
 
@@ -181,8 +184,8 @@ FBy_data = np.full((20, 20, 20), 1.0)
 # from cell 10 (index 9) to cell 11
 # reduction of 1E-3
 FBy_data[:, 9, :] = 0.001
-write_pfb(get_absolute_path('Flow_Barrier_Y.pfb'), FBy_data)
-rich_fby.dist('Flow_Barrier_Y.pfb')
+write_pfb(get_absolute_path(new_output_dir_name + '/Flow_Barrier_Y.pfb'), FBy_data)
+rich_fby.dist(new_output_dir_name + '/Flow_Barrier_Y.pfb')
 
 #-----------------------------------------------------------------------------
 # Wells
@@ -304,4 +307,22 @@ rich_fby.Solver.Linear.Preconditioner = 'PFMG'
 # Run and Unload the ParFlow output files
 #-----------------------------------------------------------------------------
 
-rich_fby.run()
+rich_fby.run(working_directory=new_output_dir_name)
+passed = True
+for i in range(11):
+    timestep = str(i).rjust(5, '0')
+    filename = f"/{run_name}.out.press.{timestep}.pfb"
+    if not pf_test_file(new_output_dir_name + filename, correct_output_dir_name + filename,
+                        f"Max difference in Pressure for timestep {timestep}"):
+        passed = False
+    filename = f"/{run_name}.out.satur.{timestep}.pfb"
+    if not pf_test_file(new_output_dir_name + filename, correct_output_dir_name + filename,
+                        f"Max difference in Saturation for timestep {timestep}"):
+        passed = False
+        
+if passed:
+    print(f"{run_name} : PASSED")
+else:
+    print(f"{run_name} : FAILED")
+    sys.exit(1)
+                
