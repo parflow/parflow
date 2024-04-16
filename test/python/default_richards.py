dda8c1159677c00f85ce1f77b4c1e17f9c925aa8
Steven Smith <smith84@llnl.gov>
2024-04-16 08:28:26 -0700

    Fix missing key in pf-keys, add test, and make file check arguments consistent with TCL version

--- test/python/default_richards.py
+++ test/python/default_richards.py
@@ -360,6 +360,7 @@ for test_file in test_files:
         passed = False
 
 
+sig_digits=6
 abs_value = 1e-12
 for i in range(6):
     timestep = str(i).rjust(5, '0')
@@ -371,15 +372,15 @@ for i in range(6):
         passed = False
     filename = f"/{run_name}.out.velx.{timestep}.pfb"
     if not pf_test_file_with_abs(new_output_dir_name + filename, correct_output_dir_name + filename,
-                                 f"Max difference in x-velocity for timestep {timestep}", abs_value):
+                                 f"Max difference in x-velocity for timestep {timestep}", sig_digits, abs_value):
         passed = False
     filename = f"/{run_name}.out.vely.{timestep}.pfb"
     if not pf_test_file_with_abs(new_output_dir_name + filename, correct_output_dir_name + filename,
-                                 f"Max difference in y-velocity for timestep {timestep}", abs_value):
+                                 f"Max difference in y-velocity for timestep {timestep}", sig_digits, abs_value):
         passed = False
     filename = f"/{run_name}.out.vely.{timestep}.pfb"
     if not pf_test_file_with_abs(new_output_dir_name + filename, correct_output_dir_name + filename,
-                                 f"Max difference in z-velocity for timestep {timestep}", abs_value):
+                                 f"Max difference in z-velocity for timestep {timestep}", sig_digits, abs_value):
         passed = False
 
 rm(new_output_dir_name)
