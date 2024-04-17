77316043227b95215744e58fe9005d35145432ab
Steven Smith <smith84@llnl.gov>
2024-04-17 15:33:08 -0700

    Release V3.13.0 (#516)

--- pftools/python/setup.py
+++ pftools/python/setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='pftools',
-    version="1.3.10",
+    version="1.3.11",
     description=('A Python package creating an interface with the ParFlow '
                  'hydrologic model.'),
     long_description=README,
