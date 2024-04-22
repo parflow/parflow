898312106ae83bb2c2b7fea5dfaac12b701b4add
Steven Smith <smith84@llnl.gov>
2024-04-22 08:28:56 -0700

    Update copyright

--- misc/bin/replace_headers_shell.pl
+++ misc/bin/replace_headers_shell.pl
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 #BHEADER**********************************************************************
 #
-#  Copyright (c) 1995-2009, Lawrence Livermore National Security,
+#  Copyright (c) 1995-2024, Lawrence Livermore National Security,
 #  LLC. Produced at the Lawrence Livermore National Laboratory. Written
 #  by the Parflow Team (see the CONTRIBUTORS file)
 #  <parflow\@lists.llnl.gov> CODE-OCEC-08-103. All rights reserved.
@@ -38,7 +38,7 @@ my $cpytxt = "";
 $cpytxt = <<END;
 #BHEADER**********************************************************************
 #
-#  Copyright (c) 1995-2009, Lawrence Livermore National Security,
+#  Copyright (c) 1995-2024, Lawrence Livermore National Security,
 #  LLC. Produced at the Lawrence Livermore National Laboratory. Written
 #  by the Parflow Team (see the CONTRIBUTORS file)
 #  <parflow\@lists.llnl.gov> CODE-OCEC-08-103. All rights reserved.
