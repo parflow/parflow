6df7a598d80effbbf806fe0cb722ff4a23459fd3
westb2 <west.bd@gmail.com>
2024-05-24 21:33:35 -0700

    Deleted more unused variables

--- test/python/tilted_v_with_reservoir.py
+++ test/python/tilted_v_with_reservoir.py
@@ -7,6 +7,7 @@ import os
 import parflow
 from parflow import Run
 from parflow.tools.fs import mkdir, cp, get_absolute_path
+from parflow.tools.compare import pf_test_file
 
 
 overland = Run("tilted_v_with_reservoir", __file__)
@@ -327,12 +328,13 @@ overland.Reservoirs.reservoir.Min_Release_Storage = 0
 overland.Reservoirs.reservoir.Release_Rate = 1
 
 dist_and_run(run_dir)
-
 correct_output_dir = get_absolute_path('../../test/correct_output')
-correct_pressure = parflow.read_pfb(correct_output_dir + "/tilted_v_with_reservoir.out.press.00010.pfb")
-our_pressure = parflow.read_pfb(f"{run_dir}/tilted_v_with_reservoir.out.press.00010.pfb")
+pf_test_file(f"{run_dir}/tilted_v_with_reservoir.out.press.00010.pfb", correct_output_dir + "/tilted_v_with_reservoir.out.press.00010.pfb", f"Max difference in Pressure for timestep 10")
 
-assert np.array_equal(correct_pressure, our_pressure)
+# correct_pressure = parflow.read_pfb(correct_output_dir + "/tilted_v_with_reservoir.out.press.00010.pfb")
+# our_pressure = parflow.read_pfb(f"{run_dir}/tilted_v_with_reservoir.out.press.00010.pfb")
+#
+# assert np.array_equal(correct_pressure, our_pressure)
 
 
 
