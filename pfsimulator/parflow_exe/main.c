da4223388233ba3e62ff51e298f7ff20507f60ff
Georgios Artavanis <gartavanis@users.noreply.github.com>
2024-12-13 16:50:08 -0500

    Cleanup rmmInit and remove old rmmFinalize

--- pfsimulator/parflow_exe/main.c
+++ pfsimulator/parflow_exe/main.c
@@ -176,15 +176,10 @@ int main(int argc, char *argv [])
     /*-----------------------------------------------------------------------
      * Initialize RMM pool allocator
      *-----------------------------------------------------------------------*/
-    //#ifdef PARFLOW_HAVE_RMM
-    // RMM
-    //    rmmOptions_t rmmOptions;
-    //    rmmOptions.allocation_mode = (rmmAllocationMode_t)(PoolAllocation | CudaManagedMemory);
-    //    rmmOptions.initial_pool_size = 1;   // size = 0 initializes half the device memory
-    //    rmmOptions.enable_logging = false;
-    //    RMM_ERR(rmmInitialize(&rmmOptions));
+
+#ifdef PARFLOW_HAVE_RMM
     rmmInit();
-    //#endif // PARFLOW_HAVE_RMM
+#endif
 
     wall_clock_time = amps_Clock();
 
@@ -496,13 +491,6 @@ int main(int argc, char *argv [])
    *-----------------------------------------------------------------------*/
 #ifdef PARFLOW_HAVE_KOKKOS
   kokkosFinalize();
-#endif
-
-  /*-----------------------------------------------------------------------
-   * Shutdown RMM pool allocator
-   *-----------------------------------------------------------------------*/
-#ifdef PARFLOW_HAVE_RMM
-  //  RMM_ERR(rmmFinalize());
 #endif
 
   return 0;
