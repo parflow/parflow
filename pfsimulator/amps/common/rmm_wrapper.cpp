9bd659a3551e30f85cf9cff3922b3c208be572d8
Georgios Artavanis <gartavanis@users.noreply.github.com>
2024-12-12 19:14:13 -0500

    Replace cuda_memory_resource with managed_memory_resource

--- pfsimulator/amps/common/rmm_wrapper.cpp
+++ pfsimulator/amps/common/rmm_wrapper.cpp
@@ -2,14 +2,15 @@
 #ifdef PARFLOW_HAVE_RMM
 
 #include <rmm/mr/device/pool_memory_resource.hpp>
+#include <rmm/mr/device/managed_memory_resource.hpp>
 
 extern "C" {
   void rmmInit() {
-    rmm::mr::cuda_memory_resource cuda_mr;
+    rmm::mr::managed_memory_resource cuda_mr;
     // Construct a resource that uses a coalescing best-fit pool allocator
     // With the pool initially all of available device memory
     auto initial_size = rmm::percent_of_free_device_memory(100);
-    rmm::mr::pool_memory_resource<rmm::mr::cuda_memory_resource> pool_mr{&cuda_mr, initial_size};
+    rmm::mr::pool_memory_resource<rmm::mr::managed_memory_resource> pool_mr{&cuda_mr, initial_size};
     rmm::mr::set_current_device_resource(&pool_mr); // Updates the current device resource pointer to `pool_mr`
   }
 
