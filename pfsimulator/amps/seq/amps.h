4763cf7eae802308890a7cca53b0b1ef144700f3
Georgios Artavanis <gartavanis@users.noreply.github.com>
2024-12-13 17:58:14 -0500

    Update RMM for amps/seq

--- pfsimulator/amps/seq/amps.h
+++ pfsimulator/amps/seq/amps.h
@@ -47,7 +47,7 @@
 #include <cuda_runtime.h>
 #endif
 #ifdef PARFLOW_HAVE_RMM
-#include <rmm/rmm_api.h>
+#include "rmm_wrapper.h"
 #endif
 
 #define AMPS_EXCHANGE_SPECIALIZED
@@ -463,24 +463,6 @@ static inline void amps_cuda_error(cudaError_t err, const char *file, int line)
 }
 #endif // PARFLOW_HAVE_CUDA || PARFLOW_HAVE_KOKKOS
 
-#ifdef PARFLOW_HAVE_RMM
-/**
- * @brief RMM error handling
- *
- * If error detected, print error message and exit.
- *
- * @param expr RMM error (of type rmmError_t) [IN]
- */
-#define RMM_ERRCHK(err) (amps_rmm_error(err, __FILE__, __LINE__))
-static inline void amps_rmm_error(rmmError_t err, const char *file, int line)
-{
-  if (err != RMM_SUCCESS)
-  {
-    printf("\n\n%s in %s at line %d\n", rmmGetErrorString(err), file, line);
-    exit(1);
-  }
-}
-#endif // PARFLOW_HAVE_RMM
 
 /*--------------------------------------------------------------------------
  * Define static unified memory allocation routines for devices
@@ -521,7 +503,7 @@ static inline void *_amps_talloc_device(size_t size)
   void *ptr = NULL;
 
 #ifdef PARFLOW_HAVE_RMM
-  RMM_ERRCHK(rmmAlloc(&ptr, size, 0, __FILE__, __LINE__));
+  ptr = rmmAlloc(size);
 #elif defined(PARFLOW_HAVE_KOKKOS)
   ptr = kokkosUVMAlloc(size);
 #elif defined(PARFLOW_HAVE_CUDA)
@@ -547,7 +529,7 @@ static inline void *_amps_ctalloc_device(size_t size)
   void *ptr = NULL;
 
 #ifdef PARFLOW_HAVE_RMM
-  RMM_ERRCHK(rmmAlloc(&ptr, size, 0, __FILE__, __LINE__));
+  ptr = rmmAlloc(size);
 #elif defined(PARFLOW_HAVE_KOKKOS)
   ptr = kokkosUVMAlloc(size);
 #elif defined(PARFLOW_HAVE_CUDA)
@@ -575,7 +557,7 @@ static inline void *_amps_ctalloc_device(size_t size)
 static inline void _amps_tfree_device(void *ptr)
 {
 #ifdef PARFLOW_HAVE_RMM
-  RMM_ERRCHK(rmmFree(ptr, 0, __FILE__, __LINE__));
+  rmmFree(ptr);
 #elif defined(PARFLOW_HAVE_KOKKOS)
   kokkosUVMFree(ptr);
 #elif defined(PARFLOW_HAVE_CUDA)
