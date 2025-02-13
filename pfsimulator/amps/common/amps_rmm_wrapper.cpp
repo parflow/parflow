947630a1b7a22778abdfba0318e739db63eddbeb
Georgios Artavanis <gartavanis@users.noreply.github.com>
2025-02-13 13:11:34 -0500

    Add comment

--- pfsimulator/amps/common/amps_rmm_wrapper.cpp
+++ pfsimulator/amps/common/amps_rmm_wrapper.cpp
@@ -1,6 +1,29 @@
 #include "amps_rmm_wrapper.h"
 #ifdef PARFLOW_HAVE_RMM
 
+/* Notes on the transition from the RMM C API to the C++ API:
+ *
+ * The RMM C++ API does not support the old rmmAlloc() and rmmFree() functions.
+ * Instead, they have implemented resource classes for the  different memory allocation
+ * algorithms. One consequence is that the deallocate() method of the pooled memory 
+ * allocator needs to know how many bytes to free. The RMM team encourages the use of
+ * containers in memory allocation in order to achieve that (such as the RMM-provided
+ * device_buffer).
+ *
+ * This is a good strategy but unfortunately, the adaptor layer of ParFlow makes this
+ * nearly impossible to implement. The scientific code is agnostic to the backend that
+ * ParFlow uses to run and makes alloc() and free() calls expecting and providing void
+ * pointers to the backend. If we started to use containers for memory allocation, we
+ * would have to either distinguish memory requests based on the backend, or modify all
+ * backends to use containers and modify the scientific code as well to work with those
+ * containers.
+ *
+ * The solution below creates those containers implicitly, by asking for PADDING more data
+ * and storing the size of the allocation in the beginning of the allocated block. PADDING
+ * is chosen so that memory alignment is not affected. 
+ *
+ */
+
 #define PADDING 128
 
 #include <rmm/mr/device/pool_memory_resource.hpp>
