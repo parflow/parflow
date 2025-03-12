e5b3e3cef5984122fc7505c5d2b53b8ebfcb2cbe
zy69guqi <richard.angersbach@fau.de>
2025-03-12 18:08:07 +0100

    Automatically mark generated function decls as extern C by enabling c_interfacing config

--- pfsimulator/third_party/pystencils/KINSpgmrAtimesDQ.py
+++ pfsimulator/third_party/pystencils/KINSpgmrAtimesDQ.py
@@ -5,6 +5,7 @@ from pystencilssfg import SourceFileGenerator, SfgConfig
 from pystencilssfg.lang.cpp import *
 
 cfg = SfgConfig()
+cfg.c_interfacing = True
 
 with SourceFileGenerator(cfg) as sfg:
     dtype = "float64"
@@ -15,21 +16,21 @@ with SourceFileGenerator(cfg) as sfg:
     update_rule_PFVProd = ps.Assignment(z.center(), x.center() * y.center())
     update_kernel_PFVProd = sfg.kernels.create(update_rule_PFVProd, "VProd_gen")
 
-    sfg.function("PyCodegen_VProd").externC()(sfg.call(update_kernel_PFVProd))
+    sfg.function("PyCodegen_VProd")(sfg.call(update_kernel_PFVProd))
 
     # DotProd = x dot y (PFVDotProd)
     r = ps.TypedSymbol("r", dtype)
     update_rule_PFVDotProd = ps.AddReductionAssignment(r, x.center() * y.center())
     update_kernel_PFVDotProd = sfg.kernels.create(update_rule_PFVDotProd, "PFVDotProd_gen")
 
-    sfg.function("PyCodegen_VDotProd").externC()(sfg.call(update_kernel_PFVDotProd))
+    sfg.function("PyCodegen_VDotProd")(sfg.call(update_kernel_PFVDotProd))
 
     # L1Norm = sum_i |x_i| (PFVL1Norm)
     r = ps.TypedSymbol("r", dtype)
     update_rule_PFVL1Norm = ps.AddReductionAssignment(r, sp.Abs(x.center()))
     update_kernel_PFVL1Norm = sfg.kernels.create(update_rule_PFVL1Norm, "PFVL1Norm_gen")
 
-    sfg.function("PyCodegen_VL1Norm").externC()(sfg.call(update_kernel_PFVL1Norm))
+    sfg.function("PyCodegen_VL1Norm")(sfg.call(update_kernel_PFVL1Norm))
 
     # z = a * x + b * y (PFVLinearSum)
     a, b = sp.symbols("a, b")
@@ -37,4 +38,4 @@ with SourceFileGenerator(cfg) as sfg:
     update_rule_PFVLinearSum = ps.Assignment(z.center(), a * x.center() + b * y.center())
     update_kernel_PFVLinearSum = sfg.kernels.create(update_rule_PFVLinearSum, "VLinearSum_gen")
 
-    sfg.function("PyCodegen_VLinearSum").externC()(sfg.call(update_kernel_PFVLinearSum))
+    sfg.function("PyCodegen_VLinearSum")(sfg.call(update_kernel_PFVLinearSum))
