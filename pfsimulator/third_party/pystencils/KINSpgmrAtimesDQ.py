f74e6d9fadc0479532335ef7ed3af9696200ec94
zy69guqi <richard.angersbach@fau.de>
2025-03-24 17:45:30 +0100

    Move FindPsystencilsSfg and use config module mechanism to facilitate porting

--- pfsimulator/third_party/pystencils/KINSpgmrAtimesDQ.py
+++ pfsimulator/third_party/pystencils/KINSpgmrAtimesDQ.py
@@ -4,33 +4,38 @@ import sympy as sp
 from pystencilssfg import SourceFileGenerator, SfgConfig
 from pystencilssfg.lang.cpp import *
 
-cfg = SfgConfig()
-cfg.c_interfacing = True
-
-with SourceFileGenerator(cfg) as sfg:
+with SourceFileGenerator() as sfg:
     dtype = "float64"
 
+    def invoke(kernel):
+        # TODO: invoke handling for CUDA
+        # if sfg.context.project_info['use_cuda']:
+        # ...
+        #else:
+        sfg.call(kernel)
+
+
     # z_i = x_i * y_i (PFVProd)
-    x, y, z = ps.fields("x, y, z: float64[3D]", layout="fzyx")
+    x, y, z = ps.fields(f"x, y, z: {dtype}[3D]", layout="fzyx")
 
     update_rule_PFVProd = ps.Assignment(z.center(), x.center() * y.center())
     update_kernel_PFVProd = sfg.kernels.create(update_rule_PFVProd, "VProd_gen")
 
-    sfg.function("PyCodegen_VProd")(sfg.call(update_kernel_PFVProd))
+    sfg.function("PyCodegen_VProd")(invoke(update_kernel_PFVProd))
 
     # DotProd = x dot y (PFVDotProd)
     r = ps.TypedSymbol("r", dtype)
     update_rule_PFVDotProd = ps.AddReductionAssignment(r, x.center() * y.center())
     update_kernel_PFVDotProd = sfg.kernels.create(update_rule_PFVDotProd, "PFVDotProd_gen")
 
-    sfg.function("PyCodegen_VDotProd")(sfg.call(update_kernel_PFVDotProd))
+    sfg.function("PyCodegen_VDotProd")(invoke(update_kernel_PFVDotProd))
 
     # L1Norm = sum_i |x_i| (PFVL1Norm)
     r = ps.TypedSymbol("r", dtype)
     update_rule_PFVL1Norm = ps.AddReductionAssignment(r, sp.Abs(x.center()))
     update_kernel_PFVL1Norm = sfg.kernels.create(update_rule_PFVL1Norm, "PFVL1Norm_gen")
 
-    sfg.function("PyCodegen_VL1Norm")(sfg.call(update_kernel_PFVL1Norm))
+    sfg.function("PyCodegen_VL1Norm")(invoke(update_kernel_PFVL1Norm))
 
     # z = a * x + b * y (PFVLinearSum)
     a, b = sp.symbols("a, b")
@@ -38,4 +43,4 @@ with SourceFileGenerator(cfg) as sfg:
     update_rule_PFVLinearSum = ps.Assignment(z.center(), a * x.center() + b * y.center())
     update_kernel_PFVLinearSum = sfg.kernels.create(update_rule_PFVLinearSum, "VLinearSum_gen")
 
-    sfg.function("PyCodegen_VLinearSum")(sfg.call(update_kernel_PFVLinearSum))
+    sfg.function("PyCodegen_VLinearSum")(invoke(update_kernel_PFVLinearSum))
---
+++ pfsimulator/third_party/pystencils/cuda_config.py
@@ -0,0 +1,13 @@
+from pystencilssfg import SfgConfig
+
+def configure_sfg(cfg: SfgConfig):
+    cfg.extensions.header = "h"
+    cfg.extensions.impl = "cu"
+    cfg.c_interfacing = True
+
+def project_info():
+    return {
+        "project_name": "pystencils_coupling",
+        "float_precision": "float64",
+        "use_cuda": True,
+    }
