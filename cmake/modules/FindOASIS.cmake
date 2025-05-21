fc46224b296715d0f2a8240317785d360f213374
kvrigor <kvrigor@users.noreply.github.com>
2025-05-21 10:50:51 +0200

    Created target OASIS3MCT::OASIS3MCT

--- cmake/modules/FindOASIS.cmake
+++ cmake/modules/FindOASIS.cmake
@@ -12,3 +12,16 @@ if(MCT_Fortran_LIBRARY AND MPEU_Fortran_LIBRARY AND SCRIP_Fortran_LIBRARY)
 endif(MCT_Fortran_LIBRARY AND MPEU_Fortran_LIBRARY AND SCRIP_Fortran_LIBRARY)
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(OASIS DEFAULT_MSG OASIS_Fortran_LIBRARY OASIS_Fortran_INCLUDES)
+
+if(OASIS_FOUND AND NOT TARGET OASIS3MCT::OASIS3MCT)
+   add_library(OASIS3MCT::OASIS3MCT INTERFACE IMPORTED)
+   target_include_directories(OASIS3MCT::OASIS3MCT INTERFACE ${OASIS_Fortran_INCLUDES})
+   target_link_libraries(OASIS3MCT::OASIS3MCT INTERFACE ${OASIS_Fortran_LIBRARY} ${MCT_Fortran_LIBRARY} ${MPEU_Fortran_LIBRARY} ${SCRIP_Fortran_LIBRARY})
+
+   # TODO: This assumes OASIS3-MCT was built with OpenMP.
+   find_package(OpenMP)
+   if (OpenMP_FOUND AND TARGET OpenMP::OpenMP_Fortran)
+     target_link_libraries(OASIS3MCT::OASIS3MCT INTERFACE OpenMP::OpenMP_Fortran)
+     message(STATUS "FindOASIS: OpenMP::OpenMP_Fortran found")
+   endif()
+endif()
