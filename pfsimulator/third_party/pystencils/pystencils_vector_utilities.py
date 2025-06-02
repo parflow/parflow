9285ccc7570bbe365bcbae9efa71968b648827ea
zy69guqi <richard.angersbach@fau.de>
2025-06-02 13:37:48 +0200

    Omit (now) unnecessary manual marking of const fields - done by pystencils now

--- pfsimulator/third_party/pystencils/pystencils_vector_utilities.py
+++ pfsimulator/third_party/pystencils/pystencils_vector_utilities.py
@@ -3,11 +3,6 @@ import sympy as sp
 import pystencils as ps
 import re
 
-from pystencils import Kernel, create_kernel
-from pystencils.codegen import Parameter, GpuKernel
-from pystencils.codegen.properties import FieldBasePtr
-from pystencils.types import PsPointerType
-from pystencils.types.quick import Fp, SInt
 from pystencilssfg import SourceFileGenerator
 from pystencilssfg.lang.gpu import cuda
 
@@ -53,57 +48,31 @@ with SourceFileGenerator() as sfg:
         return f"_data_{f}"
 
 
-    def create_kernel_func(assign, func_name: str, field_written: str = "z", allow_vect: bool = True):
+    def create_kernel_func(assign, func_name: str, allow_vect: bool = True):
         func_name = f"PyCodegen_{func_name}"
         kernel_name = f"{func_name}_gen"
-        old_kernel = create_kernel(assign, get_kernel_cfg(allow_vect))
-
-        params = []
-        missing_strides = []
-        for param in old_kernel.parameters:
-            # SIMD: extend parameter list with missing _stride_XYZ_0 parameters
-            if target.is_vector_cpu() and allow_vect:
+        kernel = sfg.kernels.create(assign, kernel_name, get_kernel_cfg(allow_vect))
+        if target.is_vector_cpu() and allow_vect:
+            # extend parameter list with missing _stride_XYZ_0 parameters
+            params = []
+            missing_strides = []
+            for i, param in enumerate(kernel.parameters):
                 pattern = re.compile('_stride_(.*)_1')
                 match = pattern.findall(param.name)
 
                 if match:
-                    stride = Parameter(f"_stride_{match[0]}_0", SInt(64))
+                    stride = sfg.var(f"_stride_{match[0]}_0", "int64")
                     params += [stride]
                     missing_strides += [stride]
-
-            # make data types of read-only fields const
-            if (
-                    param.get_properties(FieldBasePtr) and
-                    param.name != fieldptr_param_name(field_written)
-            ):
-                params += [Parameter(param.name, PsPointerType(Fp(64, const=True), restrict=True, const=True))]
-            else:
                 params += [param]
 
-        if isinstance(old_kernel, GpuKernel):
-            kernel = GpuKernel(old_kernel.body,
-                               old_kernel.target,
-                               old_kernel.name,
-                               params,
-                               old_kernel.required_headers,
-                               old_kernel._jit,
-                               old_kernel._launch_config_factory
-                               )
+            sfg.function(func_name).params(*params)(
+                # TODO: mark _stride_XYZ_0 params as unused via void cast
+                invoke(kernel)
+            )
         else:
-            kernel = Kernel(old_kernel.body,
-                            old_kernel.target,
-                            old_kernel.name,
-                            params,
-                            old_kernel.required_headers,
-                            old_kernel._jit
-                            )
-
-        khandle = sfg.kernels.add(kernel, kernel_name)
-
-        sfg.function(func_name)(
-            # TODO: mark _stride_XYZ_0 params as unused via void cast
-            invoke(khandle)
-        )
+            # no extra handling needed -> just invoke the kernel
+            sfg.function(func_name)(invoke(kernel))
 
 
     # symbols
@@ -144,22 +113,22 @@ with SourceFileGenerator() as sfg:
     create_kernel_func(ps.Assignment(z.center(), x.center() + b), "VAddConst")
 
     # Returns x dot y (PFVDotProd)
-    create_kernel_func(ps.AddReductionAssignment(r, x.center() * y.center()), "VDotProd", "")
+    create_kernel_func(ps.AddReductionAssignment(r, x.center() * y.center()), "VDotProd")
 
     # Returns ||x||_{max} (PFVMaxNorm)
-    create_kernel_func(ps.MaxReductionAssignment(r, sp.Max(x.center())), "VMaxNorm", "")
+    create_kernel_func(ps.MaxReductionAssignment(r, sp.Max(x.center())), "VMaxNorm")
 
     # Returns sum_i (x_i * w_i)^2 (PFVWrmsNormHelper)
-    create_kernel_func(ps.AddReductionAssignment(r, x.center() ** 2 * w.center() ** 2), "VWrmsNormHelper", "")
+    create_kernel_func(ps.AddReductionAssignment(r, x.center() ** 2 * w.center() ** 2), "VWrmsNormHelper")
 
     # Returns sum_i |x_i| (PFVL1Norm)
-    create_kernel_func(ps.AddReductionAssignment(r, sp.Abs(x.center())), "VL1Norm", "")
+    create_kernel_func(ps.AddReductionAssignment(r, sp.Abs(x.center())), "VL1Norm")
 
     # Returns min_i x_i (PFVMin)
-    create_kernel_func(ps.MinReductionAssignment(r, x.center()), "VMin", "")
+    create_kernel_func(ps.MinReductionAssignment(r, x.center()), "VMin")
 
     # Returns max_i x_i (PFVMax)
-    create_kernel_func(ps.MaxReductionAssignment(r, x.center()), "VMax", "")
+    create_kernel_func(ps.MaxReductionAssignment(r, x.center()), "VMax")
 
     # TODO: Implement? Not ideal target code for pystencils
     #  PFVConstrProdPos(c, x)            Returns FALSE if some c_i = 0 &
@@ -169,7 +138,7 @@ with SourceFileGenerator() as sfg:
     create_kernel_func(
         ps.Assignment(z.center(),
                       sp.Piecewise((1.0, sp.Abs(x.center()) >= c), (0.0, True))),
-        "VCompare", field_written="z", allow_vect=False)
+        "VCompare", allow_vect=False)
 
     # TODO: Implement? Not ideal target code for pystencils
     #  PFVInvTest(x, z)                  Returns (x_i != 0 forall i), z_i = 1 / x_i
@@ -199,12 +168,10 @@ with SourceFileGenerator() as sfg:
     create_kernel_func(ps.Assignment(z.center(), a * x.center() - y.center()), "VLin2")
 
     # y = y + a * x (PFVAxpy)
-    create_kernel_func(ps.Assignment(y.center(), y.center() + a * x.center()), "VAxpy",
-                       field_written="y")
+    create_kernel_func(ps.Assignment(y.center(), y.center() + a * x.center()), "VAxpy")
 
     # x = x * a (PFVScaleBy)
-    create_kernel_func(ps.Assignment(x.center(), x.center() * a), "VScaleBy",
-                       field_written="x")
+    create_kernel_func(ps.Assignment(x.center(), x.center() * a), "VScaleBy")
 
     # TODO: Implement?
     #  PFVLayerCopy (a, b, x, y)        NBE: Extracts layer b from vector y, inserts into layer a of vector x
