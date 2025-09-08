4fe0d35c596f57c36ce4b276f15b5f7523e9f6ee
tikoh-station <mannyassuncao@hotmail.com>
2025-09-08 14:44:44 +0200

    Fix: code style incongruency

--- pfsimulator/parflow_lib/problem.c
+++ pfsimulator/parflow_lib/problem.c
@@ -454,7 +454,7 @@ ProblemData   *NewProblemData(
   ProblemDataIndexOfDomainTop(problem_data) = NewVectorType(grid2d, 1, 1, vector_cell_centered_2D);
   ProblemDataPatchIndexOfDomainTop(problem_data) = NewVectorType(grid2d, 1, 1, vector_cell_centered_2D);
   ProblemDataIndexOfDomainBottom(problem_data) = NewVectorType(grid2d, 1, 1, vector_cell_centered_2D);
-  
+
   ProblemDataPorosity(problem_data) = NewVectorType(grid, 1, 1, vector_cell_centered);
 
   ProblemDataBCPressureData(problem_data) = NewBCPressureData();
