a895206839ec2a42533d5ff7e72876ba689d7744
George Artavanis <ga6@verde.princeton.edu>
2025-11-16 20:15:31 -0500

    Cleanup tests

--- test/python/overland_tiltedV_KWE_top_patch.py
+++ test/python/overland_tiltedV_KWE_top_patch.py
@@ -187,12 +187,12 @@ overland.Patch.bottom.BCPressure.Type = "FluxConst"
 overland.Patch.bottom.BCPressure.Cycle = "constant"
 overland.Patch.bottom.BCPressure.alltime.Value = 0.0
 
-overland.Patch.slope.BCPressure.Type = "OverlandFlow"
+overland.Patch.slope.BCPressure.Type = "OverlandKinematic"
 overland.Patch.slope.BCPressure.Cycle = "rainrec"
 overland.Patch.slope.BCPressure.rain.Value = -0.01
 overland.Patch.slope.BCPressure.rec.Value = 0.0000
 
-overland.Patch.channel.BCPressure.Type = "OverlandFlow"
+overland.Patch.channel.BCPressure.Type = "OverlandKinematic"
 overland.Patch.channel.BCPressure.Cycle = "rainrec"
 overland.Patch.channel.BCPressure.rain.Value = -0.001
 overland.Patch.channel.BCPressure.rec.Value = 0.0000
@@ -274,8 +274,6 @@ overland.Geom.domain.ICPressure.RefPatch = "bottom"
 runcheck = 1
 correct_output_dir_name = get_absolute_path("../correct_output")
 test_dir = get_absolute_path("test_output/overland_tiltedV_KWE_top_patch/")
-overland.Patch.slope.BCPressure.Type = "OverlandKinematic"
-overland.Patch.channel.BCPressure.Type = "OverlandKinematic"
 overland.Solver.PrintSlopes = True
 
 # Ypos
