35c2b42a8eff496ac0673c3c51640d620190fb77
Steven Smith <smith84@llnl.gov>
2026-04-21 16:22:47 -0700

    Reformat CMake files to standard with gersmi

--- cmake/modules/FindSILO.cmake
+++ cmake/modules/FindSILO.cmake
@@ -21,21 +21,29 @@ if(NOT SILO_ROOT)
     set(SILO_ROOT $ENV{SILO_ROOT})
 endif()
 
-find_path(SILO_INCLUDE_DIR NAMES silo.h
-  PATH_SUFFIXES silo
-  HINTS ${SILO_ROOT}/include
-  PATHS /usr/include /usr/local/include)
+find_path(
+    SILO_INCLUDE_DIR
+    NAMES silo.h
+    PATH_SUFFIXES silo
+    HINTS ${SILO_ROOT}/include
+    PATHS /usr/include /usr/local/include
+)
 
-find_library(SILO_LIBRARY NAMES siloxx siloh5 silo
-  HINTS ${SILO_ROOT}/lib
-  PATHS /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib)
+find_library(
+    SILO_LIBRARY
+    NAMES siloxx siloh5 silo
+    HINTS ${SILO_ROOT}/lib
+    PATHS /usr/lib64 /usr/lib /usr/local/lib64 /usr/local/lib
+)
 
 set(SILO_INCLUDE_DIRS ${SILO_INCLUDE_DIR})
 set(SILO_LIBRARIES ${SILO_LIBRARY})
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(SILO DEFAULT_MSG SILO_LIBRARIES SILO_INCLUDE_DIRS)
+find_package_handle_standard_args(
+    SILO
+    DEFAULT_MSG
+    SILO_LIBRARIES
+    SILO_INCLUDE_DIRS
+)
 
-MARK_AS_ADVANCED(SILO_INCLUDE_DIRS SILO_LIBRARIES)
-
-
-
+mark_as_advanced(SILO_INCLUDE_DIRS SILO_LIBRARIES)
--- cmake/modules/FindSZLIB.cmake
+++ cmake/modules/FindSZLIB.cmake
@@ -27,22 +27,30 @@ set(_SZLIB_SEARCHES)
 
 # Search ZLIB_ROOT first if it is set.
 if(SZLIB_ROOT)
-  set(_SZLIB_SEARCH_ROOT PATHS ${SZLIB_ROOT} NO_DEFAULT_PATH)
-  list(APPEND _SZLIB_SEARCHES _SZLIB_SEARCH_ROOT)
+    set(_SZLIB_SEARCH_ROOT PATHS ${SZLIB_ROOT} NO_DEFAULT_PATH)
+    list(APPEND _SZLIB_SEARCHES _SZLIB_SEARCH_ROOT)
 endif()
 
-set(SZLIB_NAMES sz )
+set(SZLIB_NAMES sz)
 
 # Try each search configuration.
 foreach(search ${_SZLIB_SEARCHES})
-  find_path(SZLIB_INCLUDE_DIR NAMES szlib.h ${${search}} PATH_SUFFIXES include)
+    find_path(
+        SZLIB_INCLUDE_DIR
+        NAMES szlib.h ${${search}}
+        PATH_SUFFIXES include
+    )
 endforeach()
 
 # Allow SZLIB_LIBRARY to be set manually, as the location of the zlib library
 if(NOT SZLIB_LIBRARY)
-  foreach(search ${_SZLIB_SEARCHES})
-    find_library(SZLIB_LIBRARY NAMES ${SZLIB_NAMES} ${${search}} PATH_SUFFIXES lib)
-  endforeach()
+    foreach(search ${_SZLIB_SEARCHES})
+        find_library(
+            SZLIB_LIBRARY
+            NAMES ${SZLIB_NAMES} ${${search}}
+            PATH_SUFFIXES lib
+        )
+    endforeach()
 endif()
 
 unset(SZLIB_NAMES)
@@ -50,12 +58,15 @@ unset(SZLIB_NAMES)
 mark_as_advanced(SZLIB_LIBRARY SZLIB_INCLUDE_DIR)
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(SZLIB REQUIRED_VARS SZLIB_LIBRARY SZLIB_INCLUDE_DIR)
+find_package_handle_standard_args(
+    SZLIB
+    REQUIRED_VARS SZLIB_LIBRARY SZLIB_INCLUDE_DIR
+)
 
 if(SZLIB_FOUND)
-  set(SZLIB_INCLUDE_DIRS ${SZLIB_INCLUDE_DIR})
-  
-  if(NOT SZLIB_LIBRARIES)
-    set(SZLIB_LIBRARIES ${SZLIB_LIBRARY})
-  endif(NOT SZLIB_LIBRARIES)
+    set(SZLIB_INCLUDE_DIRS ${SZLIB_INCLUDE_DIR})
+
+    if(NOT SZLIB_LIBRARIES)
+        set(SZLIB_LIBRARIES ${SZLIB_LIBRARY})
+    endif(NOT SZLIB_LIBRARIES)
 endif(SZLIB_FOUND)
