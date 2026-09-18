ab0bd148cf65e523c44b4cd117811f7c00703559
George Artavanis <ga6@verde.princeton.edu>
2026-09-18 15:10:01 -0400

    Pfformat

--- docs/user_manual/conf.py
+++ docs/user_manual/conf.py
@@ -25,7 +25,13 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.mathjax", "sphinxcontrib.bibtex", "sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx.ext.autosummary"]
+extensions = [
+    "sphinx.ext.mathjax",
+    "sphinxcontrib.bibtex",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+]
 autosummary_generate = True
 bibtex_bibfiles = ["refs.bib"]
 
